c12aae2653937b6506727b18745f24b47e98f050
Alec Aivazis <alec@aivazis.com>
2021-01-19 19:05:36 -0800

    preprocessor leaves behind imports for snowpack

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,18 @@
 const sveltePreprocess = require('svelte-preprocess')
 const mosaicPreprocessor = require('./src/mosaic/preprocessor')
+const path = require('path')
+
+const artifactDirectory = path.join(__dirname, "generated")
+const artifactDirectoryAlias = '$generated'
 
 module.exports = {
     // Consult https://github.com/sveltejs/svelte-preprocess
     // for more information about preprocessors
-    preprocess: [sveltePreprocess(), mosaicPreprocessor()],
+    preprocess: [sveltePreprocess(), mosaicPreprocessor({ artifactDirectory, artifactDirectoryAlias })],
     kit: {
         // By default, `npm run build` will create a standard Node app.
-        // You can create optimized builds for different platforms by
-        // specifying a different adapter
+        // You can() create optimized builds for different platforms by
+        // specifying a difPferent adapter
         adapter: '@sveltejs/adapter-node',
 
         // hydrate the <div id="svelte"> element in src/app.html
