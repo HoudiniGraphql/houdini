fc541e3a0b82d821d0fa9e5081717f6ae8ee8d4c
Alec Aivazis <alec@aivazis.com>
2021-01-21 10:25:00 -0800

    Tests (#1)
    * started writing tests
    
    * added test for flattening fragments

---
+++ scripts/compile.test.ts
@@ -0,0 +1,28 @@
+// locals
+import { flattenFragments, Document } from './compile'
+
+test('happy path', function () {
+	// build up the list of dependents
+	const fragments = {
+		A: {
+			requiredFragments: ['B', 'C'],
+		},
+		B: {
+			requiredFragments: ['B', 'C'],
+		},
+		C: {
+			requiredFragments: ['D'],
+		},
+		D: {
+			requiredFragments: [],
+		},
+	}
+
+	// the document we are flattening
+	const doc = {
+		requiredFragments: ['A'],
+	}
+
+	// make sure we get the expected value
+	expect(flattenFragments(doc, fragments)).toEqual(expect.arrayContaining(['A', 'B', 'C', 'D']))
+})
