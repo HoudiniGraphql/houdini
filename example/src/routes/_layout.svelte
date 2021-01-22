4d0ebabe55f1b9cabfbe6b63ee2abb76c1f3b003
Alec Aivazis <alec@aivazis.com>
2021-01-22 14:10:21 -0800

    no need for preload when setting environment

--- example/src/routes/_layout.svelte
+++ example/src/routes/_layout.svelte
@@ -1,28 +1,25 @@
 <script context="module">
 	import { setEnvironment, Environment } from 'houdini'
 
-	// configure the environment once in preload so we have access to the isomorphic this.fetch
-	export async function preload() {
-		// configure the environment to use
-		setEnvironment(
-			new Environment(async ({ text, variables }) => {
-				// send the request to the ricky and morty api
-				const result = await this.fetch('https://rickandmortyapi.com/graphql', {
-					method: 'POST',
-					headers: {
-						'Content-Type': 'application/json',
-					},
-					body: JSON.stringify({
-						query: text,
-						variables,
-					}),
-				})
-
-				// parse the result as json
-				return await result.json()
+	// configure the environment to use
+	setEnvironment(
+		new Environment(async ({ text, variables }) => {
+			// send the request to the ricky and morty api
+			const result = await this.fetch('https://rickandmortyapi.com/graphql', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({
+					query: text,
+					variables,
+				}),
 			})
-		)
-	}
+
+			// parse the result as json
+			return await result.json()
+		})
+	)
 </script>
 
 <slot />
