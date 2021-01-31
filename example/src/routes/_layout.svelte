13cdd42537d312460a04dd5c0ac03431f14063e8
Alec Aivazis <alec@aivazis.com>
2021-01-30 19:51:35 -0800

    touched up comment

--- example/src/routes/_layout.svelte
+++ example/src/routes/_layout.svelte
@@ -7,7 +7,7 @@
 		setEnvironment(
 			new Environment(({ text, variables = {} }) => {
 				// usually this would require a network request but we're going to
-				// use the schema directly here to avoid the server
+				// use the schema directly here to avoid starting the server
 				return graphql(schema, text, null, null, variables)
 			})
 		)
