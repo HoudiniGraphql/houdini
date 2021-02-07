9c565fbdd28d138da1a3e215ad6b4406c79f7e71
Alec Aivazis <alec@aivazis.com>
2021-02-07 12:18:30 -0800

    fleshed out more of the cache generation tests

--- packages/houdini-compiler/src/generators/mutations.test.ts
+++ packages/houdini-compiler/src/generators/mutations.test.ts
@@ -1,9 +1,11 @@
 // external imports
+import * as recast from 'recast'
 import path from 'path'
 import { testConfig } from 'houdini-common'
 import * as graphql from 'graphql'
 import fs from 'fs/promises'
 import mockFs from 'mock-fs'
+import { Program, ExportDefaultDeclaration, ExportNamedDeclaration } from 'estree'
 // local imports
 import runGenerators from '.'
 import { CollectedGraphQLDocument } from '../types'
@@ -51,6 +53,31 @@ test('generates cache updaters', async function () {
 			path.basename(config.interactionPath({ query: 'TestQuery', mutation: 'TestMutation' })),
 		])
 	)
+
+	// load the contents of the file
+	const contents = await fs.readFile(
+		config.interactionPath({ query: 'TestQuery', mutation: 'TestMutation' }),
+		'utf-8'
+	)
+
+	// make sure there is something
+	expect(contents).toBeTruthy()
+
+	// parse the contents
+	const parsedContents: Program = recast.parse(contents)
+	// sanity check
+	expect(parsedContents.type).toBe('Program')
+
+	// find the definition of the interaction handler
+	const handlerDefinition = parsedContents.body.filter(
+		(expression) =>
+			(expression.type === 'ExportNamedDeclaration' ||
+				expression.type === 'ExportDefaultDeclaration') &&
+			expression.declaration?.type === 'FunctionDeclaration' &&
+			expression.declaration.id.name === 'applyMutation'
+	)
+	// make sure it exists
+	expect(handlerDefinition).toBeTruthy()
 })
 
 test.skip('inline fragments in mutation body count as an intersection', function () {})
