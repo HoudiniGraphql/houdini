fce0e7a643488b55eb804666d6670463e3429ce8
Alec Aivazis <alec@aivazis.com>
2021-02-09 01:58:50 -0800

    removed unused files

--- scripts/version.js
+++
@@ -1,5 +0,0 @@
-// this script is intended to be run on a github action runner.
-// it looks for labels on the pr that initiated a workflow
-// and runs the appropriate lerna command
-
-console.log('hello world')
