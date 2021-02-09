da5b194d29358ca750e3055b8575f2323800d461
Alec Aivazis <alec@aivazis.com>
2021-02-08 22:43:30 -0800

    Publishing workflow (#15)
    * added lerna
    
    * version should be 0.2.0
    
    * v0.2.1
    
    * specify nodeLinker so yarn is happy
    
    * v0.2.2
    
    * another version update
    
    * bumped example versions
    
    * start adding version bump to release flow
    
    * more workflow tweaks
    
    * debugging log
    
    * should be interesting
    
    * echo list?
    
    * expression in echo
    
    * try that again
    
    * check pull_request not issues
    
    * better check
    
    * learn bash noob
    
    * this 'should work'
    
    * remove unnecessary predicate

---
+++ scripts/version.js
@@ -0,0 +1,5 @@
+// this script is intended to be run on a github action runner.
+// it looks for labels on the pr that initiated a workflow
+// and runs the appropriate lerna command
+
+console.log('hello world')
