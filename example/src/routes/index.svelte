fa749f4b5f44eb611322691908bcde9f7af56120
Alec Aivazis <alec@aivazis.com>
2021-02-18 16:59:52 -0800

    add redirect away from /

---
+++ example/src/routes/index.svelte
@@ -0,0 +1,9 @@
+<!-- 
+    I couldn't figure out how to only have [filter].svelte. As far as I can tell,
+    there HAS to be an index.svelte file. so let's redirect to /active
+-->
+<script context="module">
+    export async function preload(page, session) {
+      return this.redirect(301, 'active')
+    }
+</script>
