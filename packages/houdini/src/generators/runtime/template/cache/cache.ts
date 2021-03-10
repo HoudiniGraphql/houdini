48b7300cef71d56ee7fecd4945786569d8a45c63
Alec Aivazis <alec@aivazis.com>
2021-03-10 08:55:51 -0800

    embedded data uses key for id

--- packages/houdini/src/generators/runtime/template/cache/cache.ts
+++ packages/houdini/src/generators/runtime/template/cache/cache.ts
@@ -349,7 +349,7 @@ export class Cache {
 					).length > 0
 
 				// figure out the id of the new linked record
-				const linkedID = !embedded ? this.id(linkedType, value) : `${parentID}.${field}`
+				const linkedID = !embedded ? this.id(linkedType, value) : `${parentID}.${keyRaw}`
 
 				// if we are now linked to a new object we need to record the new value
 				if (oldID !== linkedID) {
@@ -380,16 +380,18 @@ export class Cache {
 				// the ids that have been added since the last time
 				const newIDs: string[] = []
 
+				// figure out if this is an embedded list or a linked one by looking for any fields marked as
+				// required to compute the entity's id in the first non-null value we can find
+				const embedded =
+					value.length > 0 &&
+					this.idFields(linkedType)?.filter(
+						(field) =>
+							typeof (value.find((val) => val) as { [key: string]: any })[field] ===
+							'undefined'
+					).length > 0
+
 				// visit every entry in the list
 				for (const [i, entry] of value.entries()) {
-					// figure out if this is an embedded list or a linked one by looking for any fields marked as
-					// required to compute the entity's id
-					const embedded =
-						this.idFields(linkedType)?.filter(
-							(field) =>
-								typeof (entry as { [key: string]: any })[field] === 'undefined'
-						).length > 0
-
 					// this has to be an object for sanity sake (it can't be a link if its a scalar)
 					if (!(entry instanceof Object) || Array.isArray(entry)) {
 						throw new Error('Encountered link to non objects')
