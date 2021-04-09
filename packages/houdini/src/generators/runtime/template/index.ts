a2a2a8869e0757322f6e168c0d248be46674f26e
Alec Aivazis <alec@aivazis.com>
2021-04-08 23:19:39 -0700

    consolidated network files

--- packages/houdini/src/generators/runtime/template/index.ts
+++ packages/houdini/src/generators/runtime/template/index.ts
@@ -3,9 +3,6 @@ import { GraphQLTagResult } from './types'
 export * from './network'
 export * from './types'
 
-// @ts-ignore: file doesn't exist yet
-export * from './environment'
-
 export { default as query, getQuery } from './query'
 export { default as mutation } from './mutation'
 export { default as fragment } from './fragment'
--- packages/houdini/src/generators/runtime/template/network.ts
+++ packages/houdini/src/generators/runtime/template/network.ts
@@ -1,6 +1,3 @@
-// @ts-ignore: file doesn't exist yet
-import type { Environment } from './environment'
-
 export type FetchParams = {
 	text: string
 	variables: { [key: string]: any }
@@ -96,4 +93,19 @@ export type SubscriptionHandler = {
 			complete: () => void
 		}
 	) => () => void
+}
+
+export class Environment {
+	private fetch: RequestHandler
+	socket: SubscriptionHandler | null | undefined
+
+	// this project uses subscriptions so make sure one is passed when constructing an environment
+	constructor(networkFn: RequestHandler, subscriptionHandler?: SubscriptionHandler | null) {
+		this.fetch = networkFn
+		this.socket = subscriptionHandler
+	}
+
+	sendRequest(ctx: FetchContext, params: FetchParams, session?: FetchSession) {
+		return this.fetch.call(ctx, params, session)
+	}
 }
