5c6505918765c80be02a7b0751f44954537a0b03
Alec Aivazis <alec@aivazis.com>
2021-04-10 00:38:09 -0700

    update sapper adapter

--- packages/houdini/src/generators/runtime/adapter.ts
+++ packages/houdini/src/generators/runtime/adapter.ts
@@ -7,14 +7,29 @@ export default async function generateAdapter(config: Config) {
 	// the location of the adapter
 	const adapterLocation = path.join(config.runtimeDirectory, 'adapter.mjs')
 
+	// figure out which adapter we need to lay down
+	const adapter = config.mode === 'sapper' ? sapperAdapter : sveltekitAdapter
+
 	// write the index file that exports the runtime
 	await fs.writeFile(adapterLocation, adapter, 'utf-8')
 }
 
-const adapter = `import { stores } from '@sapper/app'
+const sapperAdapter = `import { stores, goTo } from '@sapper/app'
 
 export function getSession() {
     return stores().session
+}
+
+export function goTo(location, options) {
+    goTo(location, options)
+}
+`
+
+const sveltekitAdapter = `import { goTo } from '$app/navigation'
+import { session } from '$app/stores'
+
+export function getSession() {
+    return session
 }
 
 export function goTo(location, options) {
