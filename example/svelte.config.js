83bf758a47915bdcd69ad131930fdf091057d8d7
Alec Aivazis <alec@aivazis.com>
2021-05-13 08:29:52 -0700

    make example work again (wrestling esm and commonjs interop)

--- example/svelte.config.js
+++ example/svelte.config.js
@@ -2,13 +2,11 @@ import preprocess from 'svelte-preprocess'
 import houdini from 'houdini-preprocess'
 import path from 'path'
 
-console.log(houdini)
-
 /** @type {import('@sveltejs/kit').Config} */
 export default {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: [preprocess(), houdini()],
+	preprocess: [preprocess(), houdini.default()],
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
