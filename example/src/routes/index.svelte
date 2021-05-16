0baf2f57c6b314405744d3590db07a130b1d91d7
Alec Aivazis <alec@aivazis.com>
2021-05-15 19:05:13 -0700

    fix comment in example index route

--- example/src/routes/index.svelte
+++ example/src/routes/index.svelte
@@ -1,6 +1,5 @@
 <!--
-    I couldn't figure out how to only have [filter].svelte. As far as I can tell,
-    there HAS to be an index.svelte file. so let's redirect to /active
+    redirect / to /all
 -->
 <script context="module">
 	export async function load() {
