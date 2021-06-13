30d8ac30d30ca1cd3970dc81f3750e06209a92a2
Alec Aivazis <alec@aivazis.com>
2021-06-13 12:34:25 -0700

    deep-equal is not esm compatible

--- packages/houdini/runtime/query.ts
+++ packages/houdini/runtime/query.ts
@@ -1,7 +1,6 @@
 // externals
 import { Readable, writable, readable } from 'svelte/store'
 import { onDestroy, onMount } from 'svelte'
-import deepEqual from 'deep-equal'
 // locals
 import { Operation, GraphQLTagResult, SubscriptionSpec, QueryArtifact } from './types'
 import cache from './cache'
@@ -77,7 +76,7 @@ export default function query<_Query extends Operation<any, any>>(
 	function writeData(newData: RequestPayload<_Query['result']>, newVariables: _Query['input']) {
 		// if the variables changed we need to unsubscribe from the old fields and
 		// listen to the new ones
-		if (subscriptionSpec && !deepEqual(variables, newVariables)) {
+		if (subscriptionSpec && JSON.stringify(variables) !== JSON.stringify(newVariables)) {
 			cache.unsubscribe(subscriptionSpec, variables)
 			cache.subscribe(subscriptionSpec, newVariables)
 		}
