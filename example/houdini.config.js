2340069598390e40aa6e127d836443eebe18573d
Alec Aivazis <alec@aivazis.com>
2021-06-15 11:05:06 -0700

    update example config file

--- example/houdini.config.js
+++ example/houdini.config.js
@@ -3,6 +3,7 @@ import path from 'path'
 export default {
 	schemaPath: path.resolve('./schema/schema.gql'),
 	sourceGlob: 'src/**/*.svelte',
-	mode: 'kit',
+	framework: 'kit',
+	module: 'esm',
 	apiUrl: 'http://localhost:4000/graphql',
 }
