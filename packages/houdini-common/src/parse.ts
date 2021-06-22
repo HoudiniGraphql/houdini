00472a5b1e9b4e27dcedfe8bf440e9b1b1d8e09e
George Crawford <g.o.crawford@gmail.com>
2021-06-22 12:56:01 +0100

    WIP

--- packages/houdini-common/src/parse.ts
+++ packages/houdini-common/src/parse.ts
@@ -1,28 +1,7 @@
-// external imports
-// import typescript from 'typescript'
-// import { parse, preprocess } from 'svelte/compiler'
-// import { Ast } from 'svelte/types/compiler/interfaces'
-
 import { parse, preprocess } from 'svelte/compiler'
 import sveltePreprocess from 'svelte-preprocess'
 
 export async function parseFile(content: string): Promise<Ast> {
-	// // in order to push the code through svelte's compiler, we need to first
-	// // handle the typescript source code
-	// const processed = await preprocess(content, [
-	// 	{
-	// 		script({ content: fileContents }) {
-	// 			const { outputText: code } = typescript.transpileModule(fileContents, {})
-	// 			return {
-	// 				code,
-	// 			}
-	// 		},
-	// 	},
-	// ])
-
-	// // run the processed source through svelte's compiler to pull out the instance and module
-	// return parse(processed.code)
-
 	const fakePreprocessor = {
 		async script({ content: input }) {
 			return {
