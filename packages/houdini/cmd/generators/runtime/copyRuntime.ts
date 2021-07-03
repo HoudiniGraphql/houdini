0f899cf855aabaf974ba6e676e2954aea0470faf
Alec Aivazis <alec@aivazis.com>
2021-07-02 23:13:35 -0700

    avoid double writing cache index file

--- packages/houdini/cmd/generators/runtime/copyRuntime.ts
+++ packages/houdini/cmd/generators/runtime/copyRuntime.ts
@@ -11,13 +11,6 @@ import { writeFile } from '../../utils'
 // @ts-ignore
 const currentDir = global.__dirname || dirname(fileURLToPath(import.meta.url))
 
-// TODO: find a better way of handling this. The runtime needs to be able to import the config file
-// 		 to support features like custom scalars. In order to pull this off, this generator
-//       copies the compiled runtime and then manually adds an import to the config file.
-//       The runtime index file already exports the config file but for some reason vite
-//       can't find the exported value when it comes from inside. It has no problem
-//       finding the config reference exported from $houdini from the preprocessor 🤷
-
 export default async function runtimeGenerator(config: Config, docs: CollectedGraphQLDocument[]) {
 	// when running in the real world, scripts are nested in a sub directory of build, in tests they aren't nested
 	// under /src so we need to figure out how far up to go to find the appropriately compiled runtime
@@ -33,27 +26,10 @@ export default async function runtimeGenerator(config: Config, docs: CollectedGr
 	)
 
 	// copy the compiled source code to the target directory
-	await recursiveCopy(source, config.runtimeDirectory)
-
-	// the path from the cache's index file to the config file
-	const cacheIndex = path.join(config.runtimeDirectory, 'cache', 'index.js')
-	const relativePath = path.relative(cacheIndex, config.filepath).slice('../'.length)
-
-	// read the cache file
-	const cacheIndexContents = await fs.readFile(cacheIndex, 'utf-8')
-
-	// define the local variable that the runtime uses to thread the config to the cache constructor
-	const newContents =
-		(config.module === 'esm'
-			? `import config from "${relativePath}"\n`
-			: `var config = require('${relativePath}');`) +
-		cacheIndexContents.replace('"use strict";', '')
-
-	// write the new cache index
-	await writeFile(cacheIndex, newContents)
+	await recursiveCopy(config, source, config.runtimeDirectory)
 }
 
-async function recursiveCopy(source: string, target: string, notRoot?: boolean) {
+async function recursiveCopy(config: Config, source: string, target: string, notRoot?: boolean) {
 	// if the folder containing the target doesn't exist, then we need to create it
 	let parentDir = path.join(target, path.basename(source))
 	// if we are at the root, then go up one
@@ -75,16 +51,42 @@ async function recursiveCopy(source: string, target: string, notRoot?: boolean) 
 				// figure out the full path of the source
 				const childPath = path.join(source, child)
 
+				// TODO: find a better way of handling this. The runtime needs to be able to import the config file
+				//       to support features like custom scalars. In order to pull this off, this generator
+				//       copies the compiled runtime and then manually adds an import to the config file.
+				//       The runtime index file already exports the config file but for some reason vite
+				//       can't find the exported value when it comes from inside. It has no problem
+				//       finding the config reference exported from $houdini from the preprocessor 🤷
+				const isCacheIndex =
+					source.substring(source.lastIndexOf('/') + 1) === 'cache' &&
+					child === 'index.js'
+				const cacheIndexPath = path.join(config.runtimeDirectory, 'cache', 'index.js')
+
 				// if the child is a directory
 				if ((await fs.lstat(childPath)).isDirectory()) {
 					// keep walking down
-					await recursiveCopy(childPath, parentDir, true)
+					await recursiveCopy(config, childPath, parentDir, true)
 				}
 				// the child is a file, copy it to the parent directory
 				else {
 					const targetPath = path.join(parentDir, child)
 
-					await writeFile(targetPath, await fs.readFile(childPath, 'utf-8'))
+					let contents = await fs.readFile(childPath, 'utf-8')
+
+					// if we are writing to the cache index file, modify the contents
+					if (isCacheIndex) {
+						const relativePath = path
+							.relative(cacheIndexPath, config.filepath)
+							.slice('../'.length)
+
+						contents =
+							(config.module === 'esm'
+								? `import config from "${relativePath}"\n`
+								: `var config = require('${relativePath}');`) +
+							contents.replace('"use strict";', '')
+					}
+
+					await writeFile(targetPath, contents)
 				}
 			})
 		)
