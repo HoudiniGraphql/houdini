a4a0f08122a345387826d7c54c9f4c688236d341
pixelmund <pixelmund@gmail.com>
2021-08-06 22:09:30 +0200

    Check for non existing cache context

--- packages/houdini/runtime/context.ts
+++ packages/houdini/runtime/context.ts
@@ -4,5 +4,15 @@ import type { Cache } from './cache/cache'
 
 export const setVariables = (vars: () => {}) => setContext('variables', vars)
 export const getVariables = (): (() => {}) => getContext('variables') || (() => ({}))
-export const getCache = () => getContext<Cache>('__houdini__cache__')
+export const getCache = () => {
+	const cache = getContext<Cache>('__houdini__cache__')
+
+	if (!cache) {
+		throw new Error(
+			'Please invoke setCache() from $houdini inside of your entry point instance script e.g. (__layout.svelte, App.svelte)'
+		)
+	}
+
+	return cache
+}
 export const setCache = () => setContext('__houdini__cache__', createCache())
