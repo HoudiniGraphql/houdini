d39b3a6ca0079abfe8c7b118b041002409fa594d
Alec Aivazis <alec@aivazis.com>
2021-08-18 21:34:06 -0700

    more gc tests

--- packages/houdini/runtime/cache/tests/gc.test.ts
+++ packages/houdini/runtime/cache/tests/gc.test.ts
@@ -41,3 +41,117 @@ test('adequate ticks of garbage collector clear unsubscribed data', function () 
 	cache.collectGarbage()
 	expect(cache.internal.record('User:1').fields).toEqual({})
 })
+
+test("subscribed data shouldn't be garbage collected", function () {
+	const cache = new Cache(testConfig())
+
+	cache.write({
+		selection: {
+			viewer: {
+				type: 'User',
+				keyRaw: 'viewer',
+				fields: {
+					id: {
+						type: 'ID',
+						keyRaw: 'id',
+					},
+					firstName: {
+						type: 'String',
+						keyRaw: 'firstName',
+					},
+				},
+			},
+		},
+		data: {
+			viewer: {
+				id: '1',
+				firstName: 'bob',
+			},
+		},
+	})
+
+	// subscribe to the fields
+	cache.subscribe({
+		rootType: 'Query',
+		selection: {
+			viewer: {
+				type: 'User',
+				keyRaw: 'viewer',
+				fields: {
+					id: {
+						type: 'ID',
+						keyRaw: 'id',
+					},
+				},
+			},
+		},
+		set: jest.fn(),
+	})
+
+	// tick the garbage collector enough times to fill up the buffer size
+	for (const _ of Array.from({ length: cache.bufferSize + 1 })) {
+		cache.collectGarbage()
+	}
+
+	expect(cache.internal.record('User:1').fields).toEqual({ id: '1' })
+})
+
+test('resubscribing to fields marked for garbage collection resets counter', function () {
+	const cache = new Cache(testConfig())
+
+	cache.write({
+		selection: {
+			viewer: {
+				type: 'User',
+				keyRaw: 'viewer',
+				fields: {
+					id: {
+						type: 'ID',
+						keyRaw: 'id',
+					},
+					firstName: {
+						type: 'String',
+						keyRaw: 'firstName',
+					},
+				},
+			},
+		},
+		data: {
+			viewer: {
+				id: '1',
+				firstName: 'bob',
+			},
+		},
+	})
+
+	// tick the gc 3 times
+	for (const _ of Array.from({ length: 3 })) {
+		cache.collectGarbage()
+	}
+
+	// subscribe to the fields
+	cache.subscribe({
+		rootType: 'Query',
+		selection: {
+			viewer: {
+				type: 'User',
+				keyRaw: 'viewer',
+				fields: {
+					id: {
+						type: 'ID',
+						keyRaw: 'id',
+					},
+				},
+			},
+		},
+		set: jest.fn(),
+	})
+
+	// tick the garbage collector enough times to fill up the buffer size
+	for (const _ of Array.from({ length: cache.bufferSize - 2 })) {
+		cache.collectGarbage()
+	}
+
+	// make sure we still have a value
+	expect(cache.internal.record('User:1').fields).toEqual({ id: '1' })
+})
