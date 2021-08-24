a1e189f5b422081cc0a9dffea538af7469580a01
Alec Aivazis <alec@aivazis.com>
2021-08-24 09:57:22 -0700

    no need for hooks

--- example/src/hooks.ts
+++
@@ -1,7 +0,0 @@
-import cache from '$houdini/runtime/cache'
-
-/** @type {import('@sveltejs/kit').Handle} */
-export async function handle({ request, render }) {
-	// we're done
-	return await render(request)
-}
