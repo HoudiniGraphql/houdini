c0c186b848a762ca32ecfe1d2e30a0cab655e3f1
Lukas Hechenberger <5802656+LukasHechenberger@users.noreply.github.com>
2021-12-10 22:21:11 +0100

    Run all tests

--- packages/houdini/runtime/cache/tests/cache.test.ts
+++ packages/houdini/runtime/cache/tests/cache.test.ts
@@ -2046,7 +2046,7 @@ describe('key evaluation', function () {
 	}
 })
 
-test.only('does not fail without data', function () {
+test('computeID does not throw without data', function () {
 	const cache = new Cache(config)
 
 	expect(cache.id('SomeType', undefined)).toEqual(undefined)
