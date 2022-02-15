f8482f94e7f6787d607fcedb9c6604152f8d91d6
Alec Aivazis <alec@aivazis.com>
2022-02-15 01:09:17 -0800

    lists can be written to and read

--- packages/houdini/runtime/cache_v2/cache.ts
+++ packages/houdini/runtime/cache_v2/cache.ts
@@ -190,7 +190,132 @@ class CacheInternal {
 				}
 			}
 			// the field could point to a list of related objects
-			else if (Array.isArray(value)) {
+			else if (
+				Array.isArray(value) &&
+				// make typescript happy
+				(typeof previousValue === 'undefined' || Array.isArray(previousValue))
+			) {
+				// make a shallow copy of the previous value we can  mutate
+				let oldIDs = [...(previousValue || [])] as (string | null)[]
+
+				// this field could be a connection (a list of references to edge objects).
+				// inserts in this list might insert objects into the connection that
+				// have already been added as part of a list operation. if that happens
+				// we will need to filter out ids that refer to these fake-edges which
+				// can be idenfitied as not having a cursor or node value
+				const emptyEdges = !update
+					? []
+					: oldIDs.filter((id) => {
+							if (!id) {
+								return false
+							}
+
+							// look up the edge record
+							const [cursorField] = this._storage.get(id, 'cursor')
+							// if there is a value for the cursor, it needs to remain
+							if (cursorField) {
+								return false
+							}
+
+							// look up the node reference
+							const [node] = this._storage.get(id, 'node')
+							// if there one, keep the edge
+							if (!node) {
+								return false
+							}
+
+							// there is no cursor
+							return true
+					  })
+
+				// if we are supposed to prepend or append and the mutation is enabled
+				// the new list of IDs for this link will start with an existing value
+
+				// build up the list of linked ids
+				let linkedIDs: LinkedList = []
+
+				// it could be a list of lists, in order to recreate the list of lists we need
+				// we need to track two sets of IDs, the ids of the embedded records and
+				// then the full structure of embedded lists. we'll use the flat list to add
+				// and remove subscribers but we'll save the second list in the record so
+				// we can recreate the structure
+				const { newIDs, nestedIDs } = this.extractNestedListIDs({
+					value,
+					abstract: Boolean(isAbstract),
+					specs: subscribersSoFar,
+					applyUpdates,
+					recordID: parent,
+					key,
+					linkedType,
+					variables: variables || {},
+					fields,
+					layer,
+				})
+
+				// if we're supposed to apply this write as an update, we need to figure out how
+				if (applyUpdates && update) {
+					// if we are updating the edges field, we might need to do a little more than just
+					// append/prepend to the field value. we might need to wrap the values in extra references
+					if (key === 'edges') {
+						// build up a list of the ids found in the new list
+						const newNodeIDs: string[] = []
+						for (const id of newIDs) {
+							if (!id) {
+								continue
+							}
+
+							// look up the lined node record
+							const [node] = this._storage.get(id, 'node')
+							// node should be a reference
+							if (typeof node !== 'string') {
+								continue
+							}
+
+							// if we dont have type information or a valid reference
+							if (!node || !this._storage.get(node, '__typename')) {
+								continue
+							}
+
+							newNodeIDs.push(node)
+						}
+
+						// only save a previous ID if the id shows up in the new list and was previously empty,
+						oldIDs = oldIDs.filter(
+							(id) => !(newIDs.includes(id as string) && emptyEdges.includes(id))
+						)
+					}
+					// if we have to prepend it, do so
+					if (update === 'prepend') {
+						linkedIDs = newIDs.concat(oldIDs as (string | null)[])
+					}
+					// otherwise we might have to append it
+					else if (update === 'append') {
+						linkedIDs = oldIDs.concat(newIDs)
+					}
+					// if the update is a replace do the right thing
+					else if (update === 'replace') {
+						linkedIDs = newIDs
+					}
+				}
+				// we're not supposed to apply this write as an update, just use the new value
+				else {
+					linkedIDs = nestedIDs as (string | null | (string | null)[])[]
+				}
+
+				// we have to notify the subscribers if a few things happen:
+				// either the data changed (ie we got new content for the same list)
+				// or we got content for a new list which could already be known. If we just look at
+				// wether the IDs are the same, situations where we have old data that
+				// is still valid would not be triggered
+				const contentChanged = JSON.stringify(linkedIDs) !== JSON.stringify(oldIDs)
+
+				// update subscriptions between the old IDs and the new ones
+
+				// if there was a change in the list
+				if (contentChanged || (oldIDs.length === 0 && newIDs.length === 0)) {
+					// update the cached value
+					layer.writeField(parent, key, linkedIDs)
+				}
 			}
 		}
 
@@ -321,6 +446,142 @@ class CacheInternal {
 
 	computeID(type: string, data: { [key: string]: GraphQLValue }) {
 		return data.id
+	}
+
+	private extractNestedListIDs({
+		value,
+		abstract,
+		recordID,
+		key,
+		linkedType,
+		fields,
+		variables,
+		applyUpdates,
+		specs,
+		layer,
+	}: {
+		value: GraphQLValue[]
+		recordID: string
+		key: string
+		linkedType: string
+		abstract: boolean
+		variables: {}
+		specs: SubscriptionSpec[]
+		applyUpdates: boolean
+		fields: SubscriptionSelection
+		layer: Layer
+	}): { nestedIDs: LinkedList; newIDs: (string | null)[] } {
+		// build up the two lists
+		const nestedIDs: LinkedList = []
+		const newIDs = []
+
+		let id = 0
+
+		for (const [i, entry] of value.entries()) {
+			// if we found another list
+			if (Array.isArray(entry)) {
+				// compute the nested list of ids
+				const inner = this.extractNestedListIDs({
+					value: entry as GraphQLValue[],
+					abstract,
+					recordID,
+					key,
+					linkedType,
+					fields,
+					variables,
+					applyUpdates,
+					specs,
+					layer,
+				})
+
+				// add the list of new ids to our list
+				newIDs.push(...inner.newIDs)
+
+				// and use the nested form in place of it
+				nestedIDs[i] = inner.nestedIDs
+				continue
+			}
+			// if the value is null just use that
+			if (entry === null || typeof entry === 'undefined') {
+				newIDs.push(null)
+				nestedIDs[i] = null
+				continue
+			}
+
+			// we know now that entry is an object
+			const entryObj = entry as GraphQLObject
+
+			// start off building up the embedded id
+			let linkedID = `${recordID}.${key}[${id++}]`
+
+			// figure out if this is an embedded list or a linked one by looking for all of the fields marked as
+			// required to compute the entity's id
+			const embedded =
+				this.idFields(linkedType)?.filter(
+					(field) => typeof (entry as GraphQLObject)[field] === 'undefined'
+				).length > 0
+
+			const typename = entryObj.__typename as string | undefined
+
+			let innerType = linkedType
+			// if we ran into an interface
+			if (abstract) {
+				// make sure we have a __typename field
+				if (!typename) {
+					throw new Error('Encountered interface type without __typename in the payload')
+				}
+
+				// we need to look at the __typename field in the response for the type
+				innerType = typename as string
+			}
+
+			// build up an
+			if (!embedded) {
+				const id = this.id(innerType, entry as {})
+				if (id) {
+					linkedID = id
+				} else {
+					continue
+				}
+			}
+
+			// if the field is marked for pagination and we are looking at edges, we need
+			// to use the underlying node for the id because the embedded key will conflict
+			// with entries in the previous loaded value.
+			// NOTE: this approach might cause weird behavior of a node is loaded in the same
+			// location in two different pages. In practice, nodes rarely show up in the same
+			// connection so it might not be a problem.
+			if (
+				key === 'edges' &&
+				entryObj.node &&
+				(entryObj.node as { __typename: string }).__typename
+			) {
+				const node = entryObj.node as {}
+				// @ts-ignore
+				const typename = node.__typename
+				let nodeID = this.id(typename, node)
+				if (nodeID) {
+					linkedID += '#' + nodeID
+				}
+			}
+
+			// update the linked fields too
+			this.writeSelection({
+				root: rootID,
+				selection: fields,
+				parent: linkedID,
+				data: entryObj,
+				variables,
+				subscribersSoFar: specs,
+				applyUpdates,
+				layer,
+			})
+
+			newIDs.push(linkedID)
+			nestedIDs[i] = linkedID
+		}
+
+		return { newIDs, nestedIDs }
 	}
 }
 
