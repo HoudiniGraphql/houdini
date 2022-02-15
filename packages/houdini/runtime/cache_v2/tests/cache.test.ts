b28fd8a577d26501a29c9a8dfcfdd953574af55f
Alec Aivazis <alec@aivazis.com>
2022-02-15 01:34:33 -0800

    tweak cache method names

--- packages/houdini/runtime/cache_v2/tests/cache.test.ts
+++ packages/houdini/runtime/cache_v2/tests/cache.test.ts
@@ -32,14 +32,14 @@ test('write selection to root', function () {
 			},
 		},
 	}
-	cache.writeSelection({
+	cache.write({
 		selection,
 		data,
 	})
 
 	// make sure we can get back what we wrote
 	expect(
-		cache.getSelection({
+		cache.read({
 			selection,
 		})
 	).toEqual({
@@ -109,7 +109,7 @@ test('linked records with updates', function () {
 	}
 
 	// write the data as a deeply nested object
-	cache.writeSelection({
+	cache.write({
 		selection: deeplyNestedSelection,
 		data: {
 			viewer: {
@@ -124,7 +124,7 @@ test('linked records with updates', function () {
 	})
 
 	// check user 1
-	expect(cache.getSelection({ selection: userFields, parent: 'User:1' })).toEqual({
+	expect(cache.read({ selection: userFields, parent: 'User:1' })).toEqual({
 		id: '1',
 		firstName: 'bob',
 		parent: {
@@ -133,14 +133,14 @@ test('linked records with updates', function () {
 	})
 
 	// check user 2
-	expect(cache.getSelection({ selection: userFields, parent: 'User:2' })).toEqual({
+	expect(cache.read({ selection: userFields, parent: 'User:2' })).toEqual({
 		id: '2',
 		firstName: 'jane',
 		parent: null,
 	})
 
 	// associate user2 with a new parent
-	cache.writeSelection({
+	cache.write({
 		selection: deeplyNestedSelection,
 		data: {
 			viewer: {
@@ -155,14 +155,14 @@ test('linked records with updates', function () {
 	})
 
 	// make sure we updated user 2
-	expect(cache.getSelection({ selection: userFields, parent: 'User:2' })).toEqual({
+	expect(cache.read({ selection: userFields, parent: 'User:2' })).toEqual({
 		id: '2',
 		firstName: 'jane-prime',
 		parent: {
 			id: '3',
 		},
 	})
-	expect(cache.getSelection({ selection: userFields, parent: 'User:3' })).toEqual({
+	expect(cache.read({ selection: userFields, parent: 'User:3' })).toEqual({
 		id: '3',
 		firstName: 'mary',
 		parent: null,
@@ -206,7 +206,7 @@ test('linked lists', function () {
 	}
 
 	// add some data to the cache
-	cache.writeSelection({
+	cache.write({
 		selection,
 		data: {
 			viewer: {
@@ -227,7 +227,7 @@ test('linked lists', function () {
 	})
 
 	// make sure we can get the linked lists back
-	expect(cache.getSelection({ selection: selection.viewer.fields, parent: 'User:1' })).toEqual({
+	expect(cache.read({ selection: selection.viewer.fields, parent: 'User:1' })).toEqual({
 		id: '1',
 		firstName: 'bob',
 		friends: [
@@ -240,6 +240,127 @@ test('linked lists', function () {
 				firstName: 'mary',
 			},
 		],
+	})
+})
+
+test('list as value with args', function () {
+	// instantiate the cache
+	const cache = new Cache(config)
+
+	// add some data to the cache
+	cache.write({
+		selection: {
+			viewer: {
+				type: 'User',
+				keyRaw: 'viewer',
+				fields: {
+					id: {
+						type: 'ID',
+						keyRaw: 'id',
+					},
+					firstName: {
+						type: 'String',
+						keyRaw: 'firstName',
+					},
+					favoriteColors: {
+						type: 'String',
+						keyRaw: 'favoriteColors(where: "foo")',
+					},
+				},
+			},
+		},
+		data: {
+			viewer: {
+				id: '1',
+				firstName: 'bob',
+				favoriteColors: ['red', 'green', 'blue'],
+			},
+		},
+	})
+
+	// look up the value
+	expect(
+		cache.read({
+			selection: {
+				favoriteColors: {
+					type: 'String',
+					keyRaw: 'favoriteColors(where: "foo")',
+				},
+			},
+			parent: 'User:1',
+		})
+	).toEqual({
+		favoriteColors: ['red', 'green', 'blue'],
+	})
+})
+
+test('root subscribe - field change', function () {
+	// instantiate a cache
+	const cache = new Cache(config)
+
+	const selection = {
+		viewer: {
+			type: 'User',
+			keyRaw: 'viewer',
+			fields: {
+				id: {
+					type: 'ID',
+					keyRaw: 'id',
+				},
+				firstName: {
+					type: 'String',
+					keyRaw: 'firstName',
+				},
+				favoriteColors: {
+					type: 'String',
+					keyRaw: 'favoriteColors',
+				},
+			},
+		},
+	}
+
+	// write some data
+	cache.write({
+		selection,
+		data: {
+			viewer: {
+				id: '1',
+				firstName: 'bob',
+				favoriteColors: ['red', 'green', 'blue'],
+			},
+		},
+	})
+
+	console.log(JSON.stringify(cache._internal_unstable._storage, null, 4))
+
+	// a function to spy on that will play the role of set
+	const set = jest.fn()
+
+	// subscribe to the fields
+	cache.subscribe({
+		rootType: 'Query',
+		selection,
+		set,
+	})
+
+	// somehow write a user to the cache with the same id, but a different name
+	cache.write({
+		selection,
+		data: {
+			viewer: {
+				id: '1',
+				firstName: 'mary',
+			},
+		},
+	})
+
+	// make sure that set got called with the full response
+	expect(set).toHaveBeenCalledWith({
+		viewer: {
+			firstName: 'mary',
+			favoriteColors: ['red', 'green', 'blue'],
+			id: '1',
+		},
 	})
 })
 
