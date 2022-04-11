34f0bb44282e28732bc9b25e780779ad9f61f23f
Alec Aivazis <alec@aivazis.com>
2022-04-11 11:58:54 -0700

    fix test path

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 export default {
 	preset: 'ts-jest',
-	projects: ['<rootDir>/packages/*'],
 	moduleFileExtensions: ['js', 'ts'],
 	snapshotSerializers: ['ts-ast-serializer'],
 	setupFilesAfterEnv: ['./jest.setup.js'],
