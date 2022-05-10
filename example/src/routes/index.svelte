86d1d634806c9840da6c5b10f011f4120c21820d
jycouet <jycouet@gmail.com>
2022-05-10 20:56:57 +0200

    :recycle: UPDATE: clean default page

--- example/src/routes/index.svelte
+++ example/src/routes/index.svelte
@@ -2,17 +2,10 @@
     redirect / to /all
 -->
 <script context="module">
-	import { KQL_AllItems } from '$houdini/stores'
 	export async function load() {
-		const result = KQL_AllItems.query()
-		// result.from => Typing OK
-
 		return {
 			redirect: 'all',
 			status: 302,
 		}
 	}
 </script>
-
-<!-- Typing OK -->
-{$KQL_AllItems.data}
