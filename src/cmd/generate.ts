7583cd3d915ae6b32099bee6f780cb67792602bb
Alec Aivazis <alec@aivazis.com>
2022-05-12 00:03:34 -0700

    can generate stores from external .graphql files

--- src/cmd/generate.ts
+++ src/cmd/generate.ts
@@ -65,118 +65,141 @@ async function collectDocuments(config: Config): Promise<CollectedGraphQLDocumen
 	const sourceFiles = await promisify(glob)(config.sourceGlob)
 
 	// the list of documents we found
-	const documents: CollectedGraphQLDocument[] = []
+	const documents: DiscoveredDoc[] = []
 
 	// wait for every file to be processed
 	await Promise.all(
-		sourceFiles.map(async (filePath) => {
+		sourceFiles.map(async (filepath) => {
 			// read the file
-			const contents = await fs.readFile(filePath, 'utf-8')
+			const contents = await fs.readFile(filepath, 'utf-8')
 
-			let parsedFile: ParsedSvelteFile
-			try {
-				parsedFile = await parseFile(contents)
-			} catch (e) {
-				const err = e as Error
-
-				// add the filepath to the error message
-				const newError = new Error(`Encountered error parsing ${filePath}: ` + err.message)
-
-				// bubble the new error up
-				throw newError
+			// if the file ends with .svelte, we need to look for graphql template tags
+			if (filepath.endsWith('.svelte')) {
+				documents.push(...(await findGraphQLTemplates(filepath, contents)))
 			}
-
-			// we need to look for multiple script tags to support sveltekit
-			const scripts = [parsedFile.instance, parsedFile.module]
-				.map((script) => (script ? script.content : null))
-				.filter(Boolean) as Program[]
-
-			await Promise.all(
-				scripts.map(async (jsContent) => {
-					// @ts-ignore
-					// look for any template tag literals in the script body
-					svelte.walk(jsContent, {
-						enter(node) {
-							// if we are looking at the graphql template tag
-							if (
-								node.type === 'TaggedTemplateExpression' &&
-								// @ts-ignore
-								node.tag.name === 'graphql'
-							) {
-								// @ts-ignore
-								// parse the tag contents to get the info we need
-								const printedDoc = node.quasi.quasis[0].value.raw
-								const parsedDoc = graphql.parse(printedDoc)
-
-								// look for the operation
-								const operations = parsedDoc.definitions.filter(
-									({ kind }) => kind === graphql.Kind.OPERATION_DEFINITION
-								)
-								// there are no operations, so its a fragment
-								const fragments = parsedDoc.definitions.filter(
-									({ kind }) => kind === graphql.Kind.FRAGMENT_DEFINITION
-								)
-								// if there is more than one operation, throw an error
-								if (operations.length > 1) {
-									throw new Error(
-										'Operation documents can only have one operation'
-									)
-								}
-								// we are looking at a fragment document
-								else {
-									// if there is more than one fragment, throw an error
-									if (fragments.length > 1) {
-										throw new Error(
-											'Fragment documents can only have one fragment'
-										)
-									}
-								}
-
-								// figure out the document kind
-								let kind = ArtifactKind.Fragment
-								if (operations.length === 1) {
-									// the document kind depends on the artifact
-
-									// query
-									if (
-										operations[0].kind === 'OperationDefinition' &&
-										operations[0].operation === 'query'
-									) {
-										kind = ArtifactKind.Query
-									}
-									// mutation
-									else if (
-										operations[0].kind === 'OperationDefinition' &&
-										operations[0].operation === 'mutation'
-									) {
-										kind = ArtifactKind.Mutation
-									}
-									// subscription
-									else if (
-										operations[0].kind === 'OperationDefinition' &&
-										operations[0].operation === 'subscription'
-									) {
-										kind = ArtifactKind.Subcription
-									}
-								}
-
-								// add it to the list
-								documents.push({
-									name: config.documentName(parsedDoc),
-									kind,
-									document: parsedDoc,
-									filename: filePath,
-									originalDocument: parsedDoc,
-									generate: true,
-								})
-							}
-						},
-					})
-				})
-			)
+			// otherwise just treat the file as a graphql file (the whole file contents constitute a graphql file)
+			else {
+				documents.push({ filepath, document: contents })
+			}
 		})
 	)
 
-	// return the list we built up
+	return await Promise.all(
+		documents.map(({ document, filepath }) =>
+			processGraphQLDocument(config, filepath, document)
+		)
+	)
+}
+
+type DiscoveredDoc = {
+	filepath: string
+	document: string
+}
+
+async function findGraphQLTemplates(filepath: string, contents: string): Promise<DiscoveredDoc[]> {
+	const documents: DiscoveredDoc[] = []
+
+	let parsedFile: ParsedSvelteFile
+	try {
+		parsedFile = await parseFile(contents)
+	} catch (e) {
+		const err = e as Error
+
+		// add the filepath to the error message
+		throw new Error(`Encountered error parsing ${filepath}: ` + err.message)
+	}
+
+	// we need to look for multiple script tags to support sveltekit
+	const scripts = [parsedFile.instance, parsedFile.module]
+		.map((script) => (script ? script.content : null))
+		.filter(Boolean) as Program[]
+
+	await Promise.all(
+		scripts.map(async (jsContent) => {
+			// @ts-ignore
+			// look for any template tag literals in the script body
+			svelte.walk(jsContent, {
+				enter(node) {
+					// if we are looking at the graphql template tag
+					if (
+						node.type === 'TaggedTemplateExpression' &&
+						// @ts-ignore
+						node.tag.name === 'graphql'
+					) {
+						// @ts-ignore
+						// parse the tag contents to get the info we need
+						const printedDoc = node.quasi.quasis[0].value.raw
+
+						documents.push({ document: printedDoc, filepath })
+					}
+				},
+			})
+		})
+	)
+
+	// we found every document in the file
 	return documents
+}
+
+async function processGraphQLDocument(
+	config: Config,
+	filepath: string,
+	document: string
+): Promise<CollectedGraphQLDocument> {
+	const parsedDoc = graphql.parse(document)
+
+	// look for the operation
+	const operations = parsedDoc.definitions.filter(
+		({ kind }) => kind === graphql.Kind.OPERATION_DEFINITION
+	)
+	// there are no operations, so its a fragment
+	const fragments = parsedDoc.definitions.filter(
+		({ kind }) => kind === graphql.Kind.FRAGMENT_DEFINITION
+	)
+	// if there is more than one operation, throw an error
+	if (operations.length > 1) {
+		throw new Error('Operation documents can only have one operation')
+	}
+	// we are looking at a fragment document
+	else {
+		// if there is more than one fragment, throw an error
+		if (fragments.length > 1) {
+			throw new Error('Fragment documents can only have one fragment')
+		}
+	}
+
+	// figure out the document kind
+	let kind = ArtifactKind.Fragment
+	if (operations.length === 1) {
+		// the document kind depends on the artifact
+
+		// query
+		if (operations[0].kind === 'OperationDefinition' && operations[0].operation === 'query') {
+			kind = ArtifactKind.Query
+		}
+		// mutation
+		else if (
+			operations[0].kind === 'OperationDefinition' &&
+			operations[0].operation === 'mutation'
+		) {
+			kind = ArtifactKind.Mutation
+		}
+		// subscription
+		else if (
+			operations[0].kind === 'OperationDefinition' &&
+			operations[0].operation === 'subscription'
+		) {
+			kind = ArtifactKind.Subcription
+		}
+	}
+
+	// add it to the list
+	return {
+		name: config.documentName(parsedDoc),
+		kind,
+		document: parsedDoc,
+		filename: filepath,
+		originalDocument: parsedDoc,
+		generate: true,
+	}
 }
