59b5d282531e1c3b00ca470a2a4e74acb240f973
jycouet <jycouet@gmail.com>
2022-05-31 01:22:47 +0200

    :bug: FIX: integration tests

--- integration/src/routes/stores/network.spec.ts
+++ integration/src/routes/stores/network.spec.ts
@@ -1,25 +1,29 @@
 import { stry } from '@kitql/helper';
 import { expect, test } from '@playwright/test';
 
-test.beforeEach(async ({ page }) => {
-  await page.goto('/stores/network');
-});
+test.beforeEach(async ({ page }) => {});
 
 test.describe('NETWORK Page', () => {
-  test('We are on the right page', async ({ page }) => {
-    expect(await page.textContent('h1')).toBe('NETWORK');
-  });
-
   test('we have no li element(s) in <ul></ul> (no data from SSR)', async ({ page, browser }) => {
+    const testData = ''; // To replace real data coming from the network
+    await page.route('**/graphql', (route) =>
+      route.fulfill({
+        status: 200,
+        body: testData
+      })
+    );
+    await page.goto('/stores/network');
+
     const ele = await page.content();
     expect(ele).toContain('<ul></ul>');
   });
 
   test('Getting the right data in a network mode', async ({ page }) => {
+    await page.goto('/stores/network');
     const res = await page.waitForResponse('http://localhost:4000/graphql');
     const json = await res.json();
     expect(stry(json, 0)).toBe(
-      '{"data":{"usersList":[{"id":"1","name":"Bruce Willis","birthDate":-466736400000},{"id":"2","name":"Samuel Jackson","birthDate":-663642000000}]}}'
+      '{"data":{"usersList":[{"id":"1","name":"Bruce Willis","birthDate":-466732800000},{"id":"2","name":"Samuel Jackson","birthDate":-663638400000}]}}'
     );
   });
 });
