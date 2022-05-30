a63d5d1d4e1c9d9423b86d5fbc6b9982c5493c86
jycouet <jycouet@gmail.com>
2022-05-31 01:59:42 +0200

    :sparkles: IMPROVE: reporter github

--- integration/playwright.config.ts
+++ integration/playwright.config.ts
@@ -1,7 +1,10 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
-  reporter: [['list'], ['html', { open: 'never' }]],
+  reporter: [['list'], ['html', { open: 'never' }], ['github']],
+  use: {
+    screenshot: 'only-on-failure'
+  },
   webServer: {
     command: 'npm run generate && npm run build && npm run preview',
     port: 3000
