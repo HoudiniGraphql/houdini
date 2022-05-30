b2d5d06d44916754cedabe7aed7af2584a724284
Alec Aivazis <alec@aivazis.com>
2022-05-29 20:23:21 -0700

    can accept extra configuration in preprocessor

--- src/preprocess/index.ts
+++ src/preprocess/index.ts
@@ -1,13 +1,14 @@
 // locals
 import { getConfig } from '../common'
+import { ConfigFile } from '../runtime'
 import applyTransforms from './transforms'
 
 // the main entry point for the preprocessor
-export default function houdiniPreprocessor() {
+export default function houdiniPreprocessor(extraConfig?: Partial<ConfigFile>) {
 	return {
 		async markup({ content, filename }: { content: string; filename: string }) {
 			// grab the config
-			const config = await getConfig()
+			const config = await getConfig(extraConfig)
 
 			// apply the transform pipeline
 			return await applyTransforms(config, { content, filename })
