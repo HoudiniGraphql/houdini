acdfa76700fd81c7616f90e92f3587fc38f6e4fb
jycouet <jycouet@gmail.com>
2022-06-05 19:07:41 +0200

    :ok_hand: REMOVE: duplicate

--- integration/src/routes/preprocess/query/variable/scalars.spec.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from '@playwright/test';
-
-test.describe('query preprocessor variables', () => {
-  test('query variables get marshaled from complex values', function () {
-    throw new Error('not yet implemented');
-  });
-});
