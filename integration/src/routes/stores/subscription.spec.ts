a0d90fb6755e7a3e4583ca286151825ae2d05220
Alec Aivazis <alec@aivazis.com>
2022-06-06 00:49:26 -0700

    add skipped tests

---
+++ integration/src/routes/stores/subscription.spec.ts
@@ -0,0 +1,7 @@
+import { expect, test } from '@playwright/test';
+
+test.describe('query store', function () {
+  test('changing variables clears subscription', function () {
+    test.skip();
+  });
+});
