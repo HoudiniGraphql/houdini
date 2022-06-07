cb9315fb5174d9a20cf67b29d4e3c784fcd8498e
Alec Aivazis <alec@aivazis.com>
2022-06-07 00:37:51 -0700

    remove unsubscribe test

--- integration/src/routes/stores/subscription.spec.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from '@playwright/test';
-
-test.describe('query store', function () {
-  test('changing variables clears subscription', function () {
-    test.skip();
-  });
-});
