bf1f5e32c72f74dc72594de78fadd29ecb19e921
Alec Aivazis <alec@aivazis.com>
2022-06-19 22:33:37 -0700

    better null page info

--- integration/src/lib/graphql/houdiniClient.ts
+++ integration/src/lib/graphql/houdiniClient.ts
@@ -19,21 +19,6 @@ async function fetchQuery({ text = '', variables = {} }, session: App.Session) {
   return await result.json();
 }
 
-// // For subscription (client only)
-// let socketClient: SubscriptionHandler | null = null
-// if (browser) {
-// 	const client = new SubscriptionClient('ws://localhost:4000/graphql', {
-// 		reconnect: true,
-// 	})
-
-// 	socketClient = {
-// 		subscribe(payload, handlers) {
-// 			const { unsubscribe } = client.request(payload).subscribe(handlers)
-// 			return unsubscribe
-// 		},
-// 	}
-// }
-
 // Export the Houdini client
 export const houdiniClient = new HoudiniClient(fetchQuery);
 // export const houdiniClient = new HoudiniClient(fetchQuery, socketClient)
