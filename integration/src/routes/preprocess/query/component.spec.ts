bf4231dc5ea1e3658b87caf0fb7e25d727436037
jycouet <jycouet@gmail.com>
2022-06-21 12:06:18 +0200

    :bug: FIX: test to get 2 responses (But still failing because the page crash)

--- integration/src/routes/preprocess/query/component.spec.ts
+++ integration/src/routes/preprocess/query/component.spec.ts
@@ -1,13 +1,13 @@
 import { test } from '@playwright/test';
 import { routes } from '../../../lib/utils/routes.js';
-import { expectGraphQLResponse, expectToBe } from '../../../lib/utils/testsHelper.js';
+import { expectNGraphQLResponse, expectToBe } from '../../../lib/utils/testsHelper.js';
 
 test.describe('query preprocessor', () => {
   test('component queries', async ({ page }) => {
     await page.goto(routes.Preprocess_query_component);
 
     // two different queries should have fired
-    await expectGraphQLResponse(page, null);
+    await expectNGraphQLResponse(page, null, 2);
 
     await expectToBe(page, 'Morgan Freeman', 'div[id=result-default]');
     await expectToBe(page, 'Samuel Jackson', 'div[id=result-prop]');
