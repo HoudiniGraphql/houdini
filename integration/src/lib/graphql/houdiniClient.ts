e365b946108c7aea290d5ea53ffce0a07037f511
jycouet <jycouet@gmail.com>
2022-06-25 19:28:47 +0200

    :ok_hand: FIX: linting

--- integration/src/lib/graphql/houdiniClient.ts
+++ integration/src/lib/graphql/houdiniClient.ts
@@ -1,15 +1,22 @@
-import { HoudiniClient, type RequestHandlerArgs } from '$houdini';
+import type { RequestHandlerArgs } from '$houdini';
+import { HoudiniClient } from '$houdini';
 import { stry } from '@kitql/helper';
 
 // For Query & Mutation
-async function fetchQuery({ fetch, text = '', variables = {}, session, metadata }: RequestHandlerArgs) {
+async function fetchQuery({
+  fetch,
+  text = '',
+  variables = {},
+  session,
+  metadata
+}: RequestHandlerArgs) {
   // // You can do what you want with your session
   // console.log(`session`, session);
 
   // // You can do what you want with your metadata at query/mutation level
   // console.log(`metadata`, metadata);
 
-  const url = import.meta.env.VITE_GRAPHQL_ENDPOINT || 'http://localhost:4000/graphql'
+  const url = import.meta.env.VITE_GRAPHQL_ENDPOINT || 'http://localhost:4000/graphql';
   const result = await fetch(url, {
     method: 'POST',
     headers: {
@@ -22,13 +29,13 @@ async function fetchQuery({ fetch, text = '', variables = {}, session, metadata 
     })
   });
 
+  // return the result as a JSON object to Houdini
   const json = await result.json();
 
   if (metadata?.logResult === true) {
     console.info(stry(json, 0));
   }
 
-  // return the result as a JSON object to Houdini
   return json;
 }
 
