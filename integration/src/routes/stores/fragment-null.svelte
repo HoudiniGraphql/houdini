387ddaf708e050a9538a68dcf802254e34a150f6
Alec Aivazis <alec@aivazis.com>
2022-07-04 19:52:24 -0700

    more logs :facepalm:

--- integration/src/routes/stores/fragment-null.svelte
+++ integration/src/routes/stores/fragment-null.svelte
@@ -1,10 +1,7 @@
 <script lang="ts">
   import UserInfoStore from '$houdini/stores/UserInfo';
-  import { MyEnum } from '$houdini';
 
   const data = UserInfoStore.get(null);
-
-  console.log(MyEnum.Value1, MyEnum.Value2);
 </script>
 
 <div id="result">
