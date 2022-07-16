e94c6f444f84f6c465a1f1bbfb5d4811bc58b0ba
jycouet <jycouet@gmail.com>
2022-07-16 11:44:31 +0200

    :boom: NEW: failing test

---
+++ integration/src/routes/layouts/layout.spec.ts
@@ -0,0 +1,27 @@
+import { expect, test } from '@playwright/test';
+import { routes } from '../../lib/utils/routes.js';
+import {
+  clientSideNavigation,
+  expectNGraphQLResponse,
+  expectNoGraphQLRequest,
+  expectToBe,
+  navSelector
+} from '../../lib/utils/testsHelper.js';
+
+test.describe('Layout & comp', () => {
+  test('From page 2 to index the store should still be filled', async ({ page }) => {
+    await page.goto(routes.Stores_Layouts_page2);
+    await expectNGraphQLResponse(page, null, 1);
+
+    const pContent = await page.locator('p').allTextContents();
+    expect(pContent).toEqual([
+      'Query Comp - Number of users: 3',
+      'Query Comp - Number of users: 3'
+    ]);
+
+    await clientSideNavigation(page, routes.Stores_Layouts);
+    await expectNoGraphQLRequest(page);
+    const pContentIndex = await page.locator('p').allTextContents();
+    expect(pContentIndex).toEqual(['Query Comp - Number of users: 3']);
+  });
+});
