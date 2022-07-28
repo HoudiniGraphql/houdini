924f21110bab69bd0064dde4afb1d715cd452774
Jaril <jarilvalenciano@gmail.com>
2022-07-28 16:29:04 -0700

    wip

--- integration/playwright.replay.config.ts
+++ integration/playwright.replay.config.ts
@@ -1,10 +1,17 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 import { devices as replayDevices } from '@replayio/playwright';
-import defaultConfig from './playwright.config.ts';
 
 const config: PlaywrightTestConfig = {
-  ...defaultConfig,
-  use: { ...(replayDevices['Replay Chromium'] as any) }
+  retries: 5,
+  reporter: process.env.CI ? [['list'], ['html', { open: 'never' }], ['github']] : [['list']],
+  use: {
+    screenshot: 'only-on-failure',
+    ...(replayDevices['Replay Chromium'] as any)
+  },
+  webServer: {
+    command: 'npm run generate && npm run build && npm run preview',
+    port: 3007
+  }
 };
 
 export default config;
