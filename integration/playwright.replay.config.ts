76cc74e0def265d44f9f09729d462b96eef1ddf5
Ryan Duffy <ryan@replay.io>
2022-07-28 16:48:53 -0700

    bump

--- integration/playwright.replay.config.ts
+++ integration/playwright.replay.config.ts
@@ -1,7 +1,7 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 import { devices as replayDevices } from '@replayio/playwright';
 import defaultConfig from './playwright.config.ts';
-
+// bump
 const config: PlaywrightTestConfig = {
   ...defaultConfig,
   use: { ...(replayDevices['Replay Chromium'] as any) }
