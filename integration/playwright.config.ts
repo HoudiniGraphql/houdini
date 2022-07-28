76fcda69deaabd8e603606c72694671384055978
Alec Aivazis <alec@aivazis.com>
2022-07-28 10:17:00 -0700

    move replay to separate config file

--- integration/playwright.config.ts
+++ integration/playwright.config.ts
@@ -1,5 +1,4 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
-import { devices as replayDevices } from '@replayio/playwright';
 
 const config: PlaywrightTestConfig = {
   retries: 5,
@@ -10,16 +9,7 @@ const config: PlaywrightTestConfig = {
   webServer: {
     command: 'npm run generate && npm run build && npm run preview',
     port: 3007
-  },
-  projects: [
-    {
-      name: 'default'
-    },
-    {
-      name: 'replay',
-      use: { ...(replayDevices['Replay Chromium'] as any) }
-    }
-  ]
+  }
 };
 
 export default config;
