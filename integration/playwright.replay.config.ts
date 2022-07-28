160eb5407df41b8792c163303d3471aabcefeec7
Ryan Duffy <ryan@replay.io>
2022-07-28 16:47:06 -0700

    revert playwright config chagnes

--- integration/playwright.replay.config.ts
+++ integration/playwright.replay.config.ts
@@ -1,17 +1,10 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 import { devices as replayDevices } from '@replayio/playwright';
+import defaultConfig from './playwright.config.ts';
 
 const config: PlaywrightTestConfig = {
-  retries: 5,
-  reporter: process.env.CI ? [['list'], ['html', { open: 'never' }], ['github']] : [['list']],
-  use: {
-    screenshot: 'only-on-failure',
-    ...(replayDevices['Replay Chromium'] as any)
-  },
-  webServer: {
-    command: 'npm run generate && npm run build && npm run preview',
-    port: 3007
-  }
+  ...defaultConfig,
+  use: { ...(replayDevices['Replay Chromium'] as any) }
 };
 
 export default config;
