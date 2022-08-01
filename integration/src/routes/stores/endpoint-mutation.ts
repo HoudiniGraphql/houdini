5b994376414cff2661b3989d66570e5f0f179c1c
jycouet <jycouet@gmail.com>
2022-08-01 17:43:58 +0200

    :clown_face: FIX: last try

--- integration/src/routes/stores/endpoint-mutation.ts
+++ integration/src/routes/stores/endpoint-mutation.ts
@@ -1,6 +1,6 @@
 import { GQL_AddUser } from '$houdini';
 
-export async function post() {
+export async function POST() {
   await GQL_AddUser.mutate({
     variables: { name: 'JYC', birthDate: new Date('1986-11-07'), delay: 200 },
     fetch
--- integration/src/routes/stores/endpoint-query.ts
+++ integration/src/routes/stores/endpoint-query.ts
@@ -1,7 +1,7 @@
 import { GQL_Hello } from '$houdini';
 import type { RequestEvent } from '@sveltejs/kit';
 
-export async function get(event: RequestEvent) {
+export async function GET(event: RequestEvent) {
   const { data } = await GQL_Hello.fetch({ event, fetch });
 
   return {
