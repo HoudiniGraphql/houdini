8eaffce32d9c6df8bc37bde6e6fbf1f5729e4734
jycouet <jycouet@gmail.com>
2022-08-01 17:16:38 +0200

    :fire: UPDATE: bump sveltekit version later

--- integration/src/routes/stores/endpoint-mutation.ts
+++ integration/src/routes/stores/endpoint-mutation.ts
@@ -1,6 +1,6 @@
 import { GQL_AddUser } from '$houdini';
 
-export async function POST() {
+export async function post() {
   await GQL_AddUser.mutate({
     variables: { name: 'JYC', birthDate: new Date('1986-11-07'), delay: 200 },
     fetch
