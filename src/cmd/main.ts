fca8ce844e0b12bb2d37061e874baebe96ff6d9c
Alec Aivazis <alec@aivazis.com>
2022-08-07 01:20:48 -0700

    add pull-schema command

--- src/cmd/main.ts
+++ src/cmd/main.ts
@@ -1,12 +1,12 @@
 import { Command } from 'commander'
 import { glob } from 'glob'
 import path from 'path'
-import util from 'util'
 
-import { formatErrors, getConfig, LogLevel, readConfigFile } from '../common'
+import { formatErrors, getConfig, LogLevel, readConfigFile, Config } from '../common'
 import { ConfigFile } from '../runtime'
 import generate from './generate'
 import init from './init'
+import pullSchema from './pullSchema'
 import { HoudiniError } from './types'
 import { writeSchema } from './utils/writeSchema'
 
@@ -53,7 +53,7 @@ program
 			}
 		) => {
 			// grab the config file
-			let config
+			let config: Config | null = null
 
 			// build up extra config values from command line arguments
 			const extraConfig: Partial<ConfigFile> = {}
@@ -61,72 +61,29 @@ program
 				extraConfig.logLevel = args.log
 			}
 
+			// load config
+			config = await getConfig(extraConfig)
+			if (args.output) {
+				config.persistedQueryPath = args.output
+			}
+			// backwards compat
+			if (args.persistOutput) {
+				console.log('⚠️ --persist-output has been replaced by --output (abbreviated -o)')
+				config.persistedQueryPath = args.persistOutput
+			}
+
 			try {
 				// Pull the newest schema if the flag is set
-				if (args.pullSchema) {
-					config = await readConfigFile()
-					// Check if apiUrl is set in config
-					if (!config.apiUrl) {
-						throw new Error(
-							'❌ Your config should contain a valid apiUrl to pull the latest schema.'
+				if (args.pullSchema && config.apiUrl) {
+					// backwards compat
+					if (args.pullHeader) {
+						console.log(
+							'⚠️ --pull-headers has been replaced by --headers (abbreviated -h)'
 						)
+						args.headers = args.pullHeader
 					}
 
-					// if the schema path is a glob, tell the user this flag doesn't do anything
-					if (config.schemaPath && glob.hasMagic(config.schemaPath)) {
-						console.warn(
-							'⚠️ --pull-schema is not supported when the schemaPath is a glob. Remember, if your ' +
-								"schema is already available locally you don't need this flag."
-						)
-					}
-					// the schema path isn't a glob
-					else {
-						// The target path -> current working directory by default. Should we allow passing custom paths?
-						const targetPath = process.cwd()
-
-						let headers = {}
-						let headerStrings: string[] = []
-
-						if (args.headers) {
-							headerStrings = args.headers
-						}
-						if (args.pullHeader) {
-							console.log(
-								'⚠️ --pull-headers has been replaced by --headers (abbreviated -h)'
-							)
-							headerStrings = args.pullHeader
-						}
-						if (headerStrings.length > 0) {
-							headers = headerStrings.reduce((total, header) => {
-								const [key, value] = header.split('=')
-								return {
-									...total,
-									[key]: value,
-								}
-							}, {})
-						}
-
-						// Write the schema
-						await writeSchema(
-							config.apiUrl,
-							config.schemaPath
-								? config.schemaPath
-								: path.resolve(targetPath, 'schema.json'),
-							headers
-						)
-					}
-				}
-
-				// Load config
-				config = await getConfig(extraConfig)
-				if (args.output) {
-					config.persistedQueryPath = args.output
-				}
-				if (args.persistOutput) {
-					console.log(
-						'⚠️ --persist-output has been replaced by --output (abbreviated -o)'
-					)
-					config.persistedQueryPath = args.persistOutput
+					await pullSchema(args)
 				}
 
 				await generate(config)
@@ -153,8 +110,18 @@ program
 		'-ph, --pull-header <headers...>',
 		'header to use when pulling your schema. Should be passed as KEY=VALUE'
 	)
-	.option('-y, --yes', 'initialize a project with the default values (compatible with SvelteKit)')
 	.action(init)
+
+// register the pull schema command
+program
+	.command('pull-schema')
+	.usage('[options]')
+	.description('pull the latest schema from your api')
+	.option(
+		'-h, --headers <headers...>',
+		'headers to use when pulling your schema. Should be passed as KEY=VALUE'
+	)
+	.action(pullSchema)
 
 // start the command
 program.parse()
---
+++ src/cmd/pullSchema.ts
@@ -0,0 +1,41 @@
+import path from 'path'
+
+import { getConfig } from '../common'
+import { writeSchema } from './utils'
+
+export default async function (args: { headers: string[] }) {
+	const config = await getConfig()
+
+	// Check if apiUrl is set in config
+	if (!config.apiUrl) {
+		throw new Error(
+			'❌ Your project does not have a remote endpoint configured. Please provide one with the `apiUrl` value in your houdini.config.js file.'
+		)
+	}
+
+	// The target path -> current working directory by default. Should we allow passing custom paths?
+	const targetPath = process.cwd()
+
+	let headers = {}
+	let headerStrings: string[] = []
+
+	if (args.headers) {
+		headerStrings = args.headers
+	}
+	if (headerStrings.length > 0) {
+		headers = headerStrings.reduce((total, header) => {
+			const [key, value] = header.split('=')
+			return {
+				...total,
+				[key]: value,
+			}
+		}, {})
+	}
+
+	// Write the schema
+	await writeSchema(
+		config.apiUrl,
+		config.schemaPath ? config.schemaPath : path.resolve(targetPath, 'schema.json'),
+		headers
+	)
+}
