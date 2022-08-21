26d41194e5d6fad957c90db71a64fb73e653397e
Alec Aivazis <alec@aivazis.com>
2022-08-20 23:45:13 -0700

    independent replay config file

--- integration/playwright.replay.config.ts
+++ integration/playwright.replay.config.ts
@@ -1,11 +1,14 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 import { devices as replayDevices } from '@replayio/playwright';
-import defaultConfig from './playwright.config';
 
 const config: PlaywrightTestConfig = {
-  ...defaultConfig,
+  reporter: process.env.CI ? [['list'], ['html', { open: 'never' }], ['github']] : [['list']],
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  use: { ...(replayDevices['Replay Chromium'] as any) }
+  use: { ...(replayDevices['Replay Chromium'] as any) },
+  webServer: {
+    command: 'npm run build && npm run preview',
+    port: 3007
+  }
 };
 
 export default config;
