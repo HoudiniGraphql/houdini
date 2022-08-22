31d4d2c0b90005de913e220fdc51d63f0ba9c9ca
jycouet <jycouet@gmail.com>
2022-08-23 00:54:15 +0200

    :clown_face: UPDATE: OMG?

--- integration/playwright.replay.config.ts
+++ integration/playwright.replay.config.ts
@@ -6,7 +6,10 @@ const config: PlaywrightTestConfig = {
   workers: 5,
   reporter: process.env.CI ? [['list'], ['html', { open: 'never' }], ['github']] : [['list']],
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  use: { ...(replayDevices['Replay Chromium'] as any) },
+  use: {
+    screenshot: 'only-on-failure'
+  },
+  // use: { ...(replayDevices['Replay Chromium'] as any) },
   webServer: {
     command: 'npm run build && npm run preview',
     port: 3007
