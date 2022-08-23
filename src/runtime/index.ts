f17e55b7e117906e775f90571bea8c40fd9b8144
Alec Aivazis <alec@aivazis.com>
2022-08-23 15:24:32 -0700

    graphql tag to return any

--- src/runtime/index.ts
+++ src/runtime/index.ts
@@ -6,8 +6,9 @@ export * from './adapter'
 export * from './stores'
 
 // this template tag gets removed by the preprocessor so it should never be invoked.
-// this function needs to return the same value as what the preprocessor leaves behind for type consistency
-export function graphql(str: TemplateStringsArray): GraphQLTagResult {
+// this function must return any so that we can assign it a type in a variable declaration (ie an inline store)
+// ideally we would be able to parse the input for values but typescript does not yet support that kind of matches in template args
+export function graphql(str: TemplateStringsArray): any {
 	// if we are executing this function as part of the plugin, we need to return
 	// the query instead of throwing an error. We don't want to bundle the graphql
 	// module into the runtime so all we can do is return the query string
