1aa9e884f340e5fd91093a02e8a7515f5c2c7df0
Alec Aivazis <alec@aivazis.com>
2022-08-24 16:57:30 -0700

    move dev deps to deps

--- site/src/app.d.ts
+++ site/src/app.d.ts
@@ -6,7 +6,7 @@ interface SessionData {}
 // for information about these interfaces
 declare namespace App {
 	interface Locals {
-		session: import('svelte-kit-cookie-session').Session<SessionData>
+		session: {}
 		cookies: Record<string, string> // all parsed cookies are automatically set from handleSession to avoid overhead
 	}
 
