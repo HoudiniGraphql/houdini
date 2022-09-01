c6bca26e098901df44ea7089c472480bdcd3e483
jycouet <jycouet@gmail.com>
2022-09-01 18:49:47 +0200

    :recycle: REMOVE: comments

--- src/runtime/lib/constants.ts
+++ src/runtime/lib/constants.ts
@@ -1,29 +1,10 @@
 export const getSiteUrl = () => {
+	// Now, manual return
 	const next_url = 'https://docs-next-kohl.vercel.app'
-	const current_url = 'https://www.houdinigraphql.com'
-
-	// in TEST, return current Url (for stable snapshots)
-	try {
-		if (process?.env?.TEST === 'true') {
-			return current_url
-		}
-	} catch (error) {
-		// This is failing in a client site navigation!
-		// That's why it's in a try catch
-	}
-
-	// Now, manual return... until automatic detection (hint bellow)
 	return next_url
 
-	// HOUDINI_VERSION is not replaced in runtime, only cmd
-	// maybe in runtime we could check meta.json?
-
-	// const version = 'HOUDINI_VERSION'
-
-	// if (version.includes('next')) {
-	// 	return 'https://docs-next-kohl.vercel.app'
-	// }
-	// return 'https://www.houdinigraphql.com'
+	// const current_url = 'https://www.houdinigraphql.com'
+	// return next_url
 }
 
 /**
