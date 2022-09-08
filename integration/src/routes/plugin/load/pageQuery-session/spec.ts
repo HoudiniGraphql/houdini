1b96389d13b44d1c96c94f75ecf812d3361a8a77
Alec Aivazis <alec@aivazis.com>
2022-09-08 15:50:49 -0700

    fix test name

--- integration/src/routes/plugin/load/pageQuery-session/spec.ts
+++ integration/src/routes/plugin/load/pageQuery-session/spec.ts
@@ -2,7 +2,7 @@ import { test } from '@playwright/test';
 import { routes } from '../../../../lib/utils/routes.js';
 import { expectToBe, expect_0_gql } from '../../../../lib/utils/testsHelper.js';
 
-test.describe('SSR Session Page', () => {
+test.describe('Page Query with Session', () => {
   test('No GraphQL request & Should display the session token', async ({ page }) => {
     await page.goto(routes.Plugin_load_pageQuerySession);
 
