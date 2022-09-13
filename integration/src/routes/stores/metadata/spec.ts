7e1e8322445f19fc38eb2636c368f80a90d4c1a1
jycouet <jycouet@gmail.com>
2022-09-13 22:34:47 +0200

    :ok_hand: FIX: test with metadata and log

--- integration/src/routes/stores/metadata/spec.ts
+++ integration/src/routes/stores/metadata/spec.ts
@@ -4,17 +4,21 @@ import { expect_1_gql, goto_expect_n_gql } from '../../../lib/utils/testsHelper.
 
 test.describe('Metadata Page', () => {
   test('Mutation => Should display the raw result in the console as info', async ({ page }) => {
-    // Go on the page
-    await goto_expect_n_gql(page, routes.Stores_Metadata, 1);
-
     // Listen to the console
     let displayed = '';
     page.on('console', (msg) => {
-      if (msg.type() === 'info') displayed = msg.text();
+      if (msg.type() === 'info') {
+        displayed = msg.text();
+      }
     });
 
+    // Go on the page
+    await goto_expect_n_gql(page, routes.Stores_Metadata, 1);
+
+    expect(displayed).toBe(`{\"data\":{\"session\":\"1234-Houdini-Token-5678\"}}`);
+
     //Click the button
-    // Mutate the data (that will be displayed in the list)
+    // Mutate the data (that will be displayed in the console)
     await expect_1_gql(page, 'button[id=mutate]');
 
     expect(displayed).toBe(
