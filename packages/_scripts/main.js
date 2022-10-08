0586e835f1f27ec49414cf5e1b2af23bf94bef95
Alec Aivazis <alec@aivazis.com>
2022-10-08 02:00:31 -0700

    fix typedef for plugins

--- packages/_scripts/main.js
+++ packages/_scripts/main.js
@@ -4,7 +4,7 @@ import build from './build.js'
 import typedefs from './typedefs.js'
 
 program.command('build').option('-p, --plugin', 'build the package as a plugin').action(build)
-program.command('typedefs').action(typedefs)
+program.command('typedefs').option('-p, --plugin', 'generate typedefs as a plugin').action(typedefs)
 
 // run the program
 program.parse()
--- packages/_scripts/typedefs.js
+++ packages/_scripts/typedefs.js
@@ -1,14 +1,19 @@
-import fs from 'fs'
+import fsSync from 'fs'
 import fs_extra from 'fs-extra'
+import fs from 'fs/promises'
 import glob from 'glob-promise'
 import path from 'path'
 import ts from 'typescript'
 
 const { ModuleResolutionKind } = ts
-const tsConfig = JSON.parse(fs.readFileSync('../../tsconfig.json', 'utf-8'))
+const tsConfig = JSON.parse(fsSync.readFileSync('../../tsconfig.json', 'utf-8'))
 
 // we'll generate the types for every file in the package in one go
-export default async function generate_typedefs() {
+export default async function generate_typedefs({ plugin }) {
+	const package_json = JSON.parse(
+		await fs.readFile(path.join(process.cwd(), 'package.json'), 'utf-8')
+	)
+
 	// grab any non-tests file
 	const files = (await glob('./src/**/*.ts', { nodir: true })).filter(
 		(path) => !path.endsWith('.test.ts')
@@ -24,10 +29,35 @@ export default async function generate_typedefs() {
 		lib: ['lib.es2021.d.ts', 'lib.dom.d.ts', 'lib.es2021.string.d.ts'],
 	})
 
+	// if we generated typedefs for a plugin, it referenced houdini and needs to be pulled out
+	let nested_typedefs = false
+	let plugin_dir = path.resolve('.', 'build', package_json.name, 'src')
+	try {
+		await fs.stat(plugin_dir)
+		nested_typedefs = true
+	} catch {}
+	if (plugin && nested_typedefs) {
+		// every directory in 'build/<package name>/src' needs to be moved into build
+		for (const child of await fs.readdir(plugin_dir)) {
+			await fs_extra.move(
+				path.join(plugin_dir, child),
+				path.join(plugin_dir, '..', '..', child),
+				{
+					overwrite: true,
+				}
+			)
+		}
+		fsSync.rmSync(path.resolve('.', 'build', 'houdini'), { recursive: true, force: true })
+		fsSync.rmSync(path.resolve('.', 'build', package_json.name), {
+			recursive: true,
+			force: true,
+		})
+	}
+
 	// if we have a runtime directory, we need to copy the typedefs we just generated into the
 	// different modules
 	const runtime_dir = path.resolve('./build/runtime')
-	if (fs.existsSync(runtime_dir)) {
+	if (fsSync.existsSync(runtime_dir)) {
 		await Promise.all(
 			['esm', 'cjs'].map((which) =>
 				fs_extra.copy(runtime_dir, path.resolve('build', `runtime-${which}`), {
