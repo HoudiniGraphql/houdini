8cd9b026cdeeb5be3ac83f9d2ecf800833cd0a08
Alec Aivazis <alec@aivazis.com>
2022-10-08 02:22:26 -0700

    remove more lint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,6 +2,7 @@ module.exports = {
 	root: true,
 	extends: '@theguild',
 	rules: {
+		'@typescript-eslint/triple-slash-reference': 'off',
 		'@typescript-eslint/no-namespace': 'off',
 		'@typescript-eslint/no-unused-vars': 'off',
 		'@typescript-eslint/ban-ts-comment': 'off',
