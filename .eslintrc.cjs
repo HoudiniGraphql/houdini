cae2bbdd6609da213228a81dcbe7fb93c985fc39
Alec Aivazis <alec@aivazis.com>
2022-10-08 01:24:00 -0700

    turn off  @typescript-eslint/no-namespace

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,6 +2,7 @@ module.exports = {
 	root: true,
 	extends: '@theguild',
 	rules: {
+		'@typescript-eslint/no-namespace': 'off',
 		'@typescript-eslint/no-unused-vars': 'off',
 		'@typescript-eslint/ban-ts-comment': 'off',
 		'@typescript-eslint/no-non-null-assertion': 'off',
