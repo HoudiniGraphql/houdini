d40269141b1e8f8aeff614c639fd8b42ee4801e6
Alec Aivazis <alec@aivazis.com>
2022-10-11 17:21:07 -0700

    update tests

--- packages/houdini/src/codegen/generators/runtime/runtime.test.ts
+++ packages/houdini/src/codegen/generators/runtime/runtime.test.ts
@@ -8,23 +8,6 @@ import { runPipeline } from '../..'
 import { fs } from '../../../lib'
 import { testConfig } from '../../../test'
 
-test('updates the network file with the client path', async function () {
-	const config = testConfig({ module: 'esm' })
-	// execute the generator
-	await runPipeline(config, [])
-
-	// open up the index file
-	const fileContents = await fs.readFile(path.join(config.runtimeDirectory, 'lib', 'network.js'))
-	expect(fileContents).toBeTruthy()
-	// parse the contents
-	const parsedQuery: ProgramKind = recast.parse(fileContents!, {
-		parser: typeScriptParser,
-	}).program
-
-	// verify contents
-	expect(recast.print(parsedQuery).code).toContain(config.client)
-})
-
 test('updates the config file with import path', async function () {
 	const config = testConfig({ module: 'esm' })
 	// execute the generator
