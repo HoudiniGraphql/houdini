497f8f9b1373e57245f77970bebbc56e6b038ad6
Alec Aivazis <alec@aivazis.com>
2022-10-25 11:41:35 -0700

    delete next and react e2e

--- e2e/react-vite/src/App.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-function App() {
-	return <div>hello</div>
-}
-
-export default App
--- e2e/react-vite/vite.config.js
+++
@@ -1,8 +0,0 @@
-import react from '@vitejs/plugin-react'
-import houdini from 'houdini/vite'
-import { defineConfig } from 'vite'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [houdini(), react()],
-})
