cf4212acf8ba4880c94d85037cad4af2b026127c
jycouet <jycouet@gmail.com>
2022-11-10 13:05:45 +0100

    :white_check_mark: UPDATE: with tests

---
+++ e2e/sveltekit/src/routes/isFetching/with_load/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { graphql } from '$houdini';
+
+  const store = graphql`
+    query isFetching_w {
+      user(id: 1, snapshot: "isFetching_w", delay: 200) {
+        id
+        name
+      }
+    }
+  `;
+
+  $: console.log(`with_load - isFetching: ${$store.isFetching}`);
+</script>
+
+<pre>{JSON.stringify($store, null, 2)}</pre>
