e486d61ca0a7a53faf185131f4945c4ba0de8e7d
Alec Aivazis <alec@aivazis.com>
2022-11-17 12:07:02 -0800

    fix unused warnings

--- e2e/sveltekit/src/params/integer.js
+++ e2e/sveltekit/src/params/integer.js
@@ -1,4 +1,6 @@
 import { MyEnum } from '$houdini';
+// this is here to silence the warning that MyEnum isn't being used :facepalm:
+MyEnum;
 
 /** @type {import('@sveltejs/kit').ParamMatcher} */
 export function match(param) {
