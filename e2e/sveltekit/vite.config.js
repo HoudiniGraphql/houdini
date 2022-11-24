e222622b8609f3439ebf1b190ad0df90ec10272e
jycouet <jycouet@gmail.com>
2022-11-25 00:31:05 +0100

    :art: UPDATE: lib reporter to light report & graph option

--- e2e/sveltekit/vite.config.js
+++ e2e/sveltekit/vite.config.js
@@ -7,42 +7,41 @@ const config = {
   plugins: [
     houdini(),
     sveltekit(),
-    libReporter({
-      name: 'houdini',
-      includes: ['$houdini/runtime', 'houdini.config.js'],
-      excludes: ['vite/preload-helper'],
-      limit: {
-        source: {
-          nb_file_max: 19 // Just to make sure we don't add a file by mistake
-        },
-        treeshaked: {
-          compressed_max: 11.5
+
+    // This plugin is checking build sizes by lib.
+    // It's not required for Houdini to work.
+    libReporter([
+      {
+        name: 'houdini',
+        includes: ['$houdini/runtime', 'houdini.config.js'],
+        excludes: ['vite/preload-helper'],
+        limit: {
+          treeshaked: {
+            compressed_max: 11.5
+          }
+        }
+      },
+      {
+        name: 'houdini-svelte',
+        includes: ['$houdini/plugins/houdini-svelte/runtime', 'src/lib/graphql/houdiniClient.ts'],
+        excludes: ['vite/preload-helper', '$houdini/runtime', '$houdini/index.js', 'svelte'],
+        limit: {
+          treeshaked: {
+            compressed_max: 8
+          }
+        }
+      },
+      {
+        name: 'houdini-full-e2e',
+        includes: ['$houdini', 'src/lib/graphql/houdiniClient.ts', 'houdini.config.js'],
+        excludes: ['vite/preload-helper', 'svelte'],
+        limit: {
+          treeshaked: {
+            compressed_max: 60
+          }
         }
       }
-    }),
-    libReporter({
-      name: 'houdini-svelte',
-      includes: ['$houdini/plugins/houdini-svelte/runtime', 'src/lib/graphql/houdiniClient.ts'],
-      excludes: ['vite/preload-helper', '$houdini/runtime', '$houdini/index.js', 'svelte'],
-      limit: {
-        source: {
-          nb_file_max: 18 // Just to make sure we don't add a file by mistake
-        },
-        treeshaked: {
-          compressed_max: 8
-        }
-      }
-    }),
-    libReporter({
-      name: 'houdini-full-e2e',
-      includes: ['$houdini', 'src/lib/graphql/houdiniClient.ts', 'houdini.config.js'],
-      excludes: ['vite/preload-helper', 'svelte'],
-      limit: {
-        treeshaked: {
-          compressed_max: 60 // Growing with the number of stores & co. We want to increase it manually
-        }
-      }
-    })
+    ])
   ]
 };
 
