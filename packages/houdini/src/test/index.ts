ba997a4a3aaf9aa5f6f501769c152ba27fb4b804
Alec Aivazis <alec@aivazis.com>
2022-12-07 16:56:22 -0800

    can read/write scalar values

--- packages/houdini/src/test/index.ts
+++ packages/houdini/src/test/index.ts
@@ -162,6 +162,9 @@ export function testConfigFile(config: Partial<ConfigFile> = {}): ConfigFile {
 			DateTime: {
 				type: 'Date',
 				unmarshal(val: number): Date {
+					if (typeof val !== 'number') {
+						throw new Error('unmarshaling not a number')
+					}
 					return new Date(val)
 				},
 				marshal(date: Date): number {
@@ -184,6 +187,7 @@ export function testConfigFile(config: Partial<ConfigFile> = {}): ConfigFile {
 				client: './my/client/path',
 			},
 		},
+		acceptImperativeInstability: true,
 		...config,
 	}
 }
