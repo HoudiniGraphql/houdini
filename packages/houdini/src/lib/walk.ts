7332bfb5d69e73a6bd2a9381d50da180a3ef5f3c
Alec Aivazis <alec@aivazis.com>
2022-12-16 20:38:39 -0800

    find_graphql looks for graphql function

--- packages/houdini/src/lib/walk.ts
+++ packages/houdini/src/lib/walk.ts
@@ -1,4 +1,8 @@
-import type { TaggedTemplateExpressionKind, IdentifierKind } from 'ast-types/lib/gen/kinds'
+import type {
+	TaggedTemplateExpressionKind,
+	IdentifierKind,
+	CallExpressionKind,
+} from 'ast-types/lib/gen/kinds'
 import { asyncWalk, BaseNode } from 'estree-walker'
 import * as graphql from 'graphql'
 
@@ -40,65 +44,101 @@ export async function find_graphql(
 ): Promise<void> {
 	await asyncWalk(parsedScript!, {
 		async enter(node, parent) {
-			// if we are looking at the graphql template tag
-			if (
-				node.type === 'TaggedTemplateExpression' &&
-				((node as TaggedTemplateExpressionKind).tag as IdentifierKind).name === 'graphql'
-			) {
+			// graphql documents come in a few forms:
+			// - graphql template tags
+			// - strings passed to graphql function
+			if (node.type !== 'TaggedTemplateExpression' && node.type !== 'CallExpression') {
+				return
+			}
+
+			let documentString: string
+
+			// process template tags
+			if (node.type === 'TaggedTemplateExpression') {
+				// grab the string passed to the template tag as the document
 				const expr = node as TaggedTemplateExpressionKind
-				// we're going to replace the tag with something the runtime can use
 
-				// first, lets parse the tag contents to get the info we need
-				const tagContent = expr.quasi.quasis[0].value.raw
-				const parsedTag = graphql.parse(tagContent)
-
-				// if there is a predicate and the graphql tag does not satisfy it
-				if (walker.where && !walker.where(parsedTag)) {
-					// ignore the tag
+				// we only care about graphql template tags
+				if (expr.tag.type !== 'Identifier' || expr.tag.name !== 'graphql') {
 					return
 				}
 
-				// pull out the name of the thing
-				const definition = config.extractDefinition(parsedTag) as
-					| graphql.OperationDefinitionNode
-					| graphql.FragmentDefinitionNode
-				const name = definition.name?.value
-				if (!name) {
-					throw new Error('Could not find definition name')
-				}
-				let kind: CompiledDocumentKind
-				if (definition.kind === 'FragmentDefinition') {
-					kind = CompiledFragmentKind
-				} else {
-					if (definition.operation === 'query') {
-						kind = CompiledQueryKind
-					} else if (definition.operation === 'mutation') {
-						kind = CompiledMutationKind
-					} else {
-						kind = CompiledSubscriptionKind
-					}
-				}
-
-				// tell the walk there was a dependency
-				walker.dependency?.(config.artifactPath(parsedTag))
-
-				// invoker the walker's callback with the right context
-				await walker.tag({
-					parsedDocument: parsedTag,
-					node: {
-						...node,
-						...this,
-						remove: this.remove,
-						replaceWith: this.replace,
-					},
-					artifact: {
-						name,
-						kind,
-					},
-					parent,
-					tagContent,
-				})
+				documentString = expr.quasi.quasis[0].value.raw
 			}
+			// process function calls
+			else if (node.type === 'CallExpression') {
+				const expr = node as CallExpressionKind
+				// if the function is not called graphql, ignore it
+				if (
+					expr.callee.type !== 'Identifier' ||
+					expr.callee.name !== 'graphql' ||
+					expr.arguments.length !== 1
+				) {
+					return
+				}
+				const argument = expr.arguments[0]
+
+				// if we have a template or string literal, use its value
+				if (argument.type === 'TemplateLiteral') {
+					documentString = argument.quasis[0].value.raw
+				} else if (argument.type === 'StringLiteral') {
+					documentString = argument.value
+				} else {
+					return
+				}
+			} else {
+				return
+			}
+
+			// if we got this far, {documentString} holds the query
+			const parsedTag = graphql.parse(documentString)
+
+			// if there is a predicate and the graphql tag does not satisfy it
+			if (walker.where && !walker.where(parsedTag)) {
+				// ignore the tag
+				return
+			}
+
+			// pull out the name of the thing
+			const definition = config.extractDefinition(parsedTag) as
+				| graphql.OperationDefinitionNode
+				| graphql.FragmentDefinitionNode
+			const name = definition.name?.value
+			if (!name) {
+				throw new Error('Could not find definition name')
+			}
+			let kind: CompiledDocumentKind
+			if (definition.kind === 'FragmentDefinition') {
+				kind = CompiledFragmentKind
+			} else {
+				if (definition.operation === 'query') {
+					kind = CompiledQueryKind
+				} else if (definition.operation === 'mutation') {
+					kind = CompiledMutationKind
+				} else {
+					kind = CompiledSubscriptionKind
+				}
+			}
+
+			// tell the walk there was a dependency
+			walker.dependency?.(config.artifactPath(parsedTag))
+
+			// invoker the walker's callback with the right context
+			await walker.tag({
+				parsedDocument: parsedTag,
+				node: {
+					...node,
+					...this,
+					remove: this.remove,
+					replaceWith: this.replace,
+				},
+				artifact: {
+					name,
+					kind,
+				},
+				parent,
+				tagContent: documentString,
+			})
 		},
 	})
 }
