0f08c441387c2a47e371a5ad999f861798e12914
Alec Aivazis <alec@aivazis.com>
2022-12-18 20:14:24 -0800

    remove unnecessary Promise.all

--- packages/houdini-plugin-svelte-global-stores/src/plugin/codegen/index.ts
+++ packages/houdini-plugin-svelte-global-stores/src/plugin/codegen/index.ts
@@ -5,10 +5,10 @@ import stores from './stores'
 
 export default async function (input: PluginGenerateInput) {
 	// create the static directories
-	await Promise.all([fs.mkdirp(global_stores_directory(input.plugin_root))])
+	await fs.mkdirp(global_stores_directory(input.plugin_root))
 
 	// generate the files
-	await Promise.all([stores(input)])
+	await stores(input)
 }
 
 export type PluginGenerateInput = Omit<GenerateHookInput, 'config'> & {
