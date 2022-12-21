7612841d9b72817bc1509ea4d593cfa74d0772c7
jycouet <jycouet@gmail.com>
2022-12-21 17:00:03 +0100

    :pencil2: DOC: tweaks

---
+++ e2e/sveltekit/src/routes/stores/action-mutation/+page.server.js
@@ -0,0 +1,26 @@
+import { graphql } from '$houdini';
+import { fail } from '@sveltejs/kit';
+
+/** @type {import('./$types').Actions} */
+export const actions = {
+  add: async (event) => {
+    const data = await event.request.formData();
+
+    const name = data.get('name')?.toString();
+
+    if (!name) {
+      return fail(403, { name: '*' });
+    }
+
+    const actionMutation = graphql(`
+      mutation ActionMutation($name: String!) {
+        addUser(name: $name, birthDate: 254143016000, snapshot: "ActionMutation") {
+          id
+          name
+        }
+      }
+    `);
+
+    return await actionMutation.mutate({ name });
+  }
+};
---
+++ e2e/sveltekit/src/routes/stores/action-mutation/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { enhance } from '$app/forms';
+
+  /** @type {import('./$types').ActionData} */
+  export let form;
+</script>
+
+<form method="POST" action="?/add" use:enhance>
+  <label>
+    Name
+    <input name="name" type="name" />
+  </label>
+  <span id="name-error"
+    >{#if form?.name}{form?.name}{/if}</span
+  >
+  <button type="submit">Add</button>
+</form>
+
+<div id="result">
+  {form?.addUser?.name || 'No user added'}
+</div>
