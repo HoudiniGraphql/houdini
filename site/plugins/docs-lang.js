304f3f03669f705ec50dad961afde71d39d9e7e5
Alec Aivazis <alec@aivazis.com>
2022-12-25 16:02:58 -0500

    transform handles variable declaration type signatures

--- site/plugins/docs-lang.js
+++ site/plugins/docs-lang.js
@@ -2,6 +2,11 @@
 // the UI will hide/show the appropriate one depending on which language the user
 // has selected
 
+import * as recast from 'recast'
+import * as typeScriptParser from 'recast/parsers/typescript.js'
+
+const AST = recast.types.builders
+
 export default {
 	async markup({ content, filename }) {
 		// only consider .svx files
@@ -27,17 +32,11 @@ export default {
 				// we actually need to treat the block as ending 2 indices later
 				endIndex += 3
 
-				// we found the end of the code block
-				let codeBlock = content.slice(match.index, endIndex)
-
-				// the first thing we need to do is mark this as a javascript plugin instead
-				codeBlock = codeBlock.replace('```typescript', '```typescript')
-				codeBlock = codeBlock.replace('.ts', '.js')
-
 				// push the new block at the beginning
 				newBlocks.unshift({
 					index: endIndex,
-					block: codeBlock
+					// transform the typescript source into the javascript equivalent
+					block: await transformTypescript(content.slice(match.index, endIndex))
 				})
 
 				// we're done processing this block
@@ -55,4 +54,84 @@ export default {
 			code: final
 		}
 	}
+}
+
+export async function transformTypescript(content) {
+	// the first thing we need to do is mark this as a javascript plugin instead
+	content = content.replace('```typescript', '```javascript')
+	content = content.replace('.ts', '.js')
+
+	// the actual source is between the first and last line break. we'll leave the first line break
+	// in place so that we can use them as markers for every line in the following loop
+	const start = content.indexOf('\n')
+	const end = content.lastIndexOf('\n')
+	const source = content.substring(start, end)
+
+	// we're going to build up the new version as a separate list
+	const transformed = []
+
+	// keep track of every type import and where it comes from so we can add comments
+	// to the javascript source
+	const importSource = {}
+
+	// loop over top level statement
+	for (const statement of await parseJS(source).body) {
+		// if the statement is a type import, we need to track where it came from
+		// and not add it to the final result
+		if (statement.type === 'ImportDeclaration' && statement.importKind === 'type') {
+			let source = statement.source.value
+			for (const { imported } of statement.specifiers) {
+				importSource[imported.name] = source
+			}
+			continue
+		}
+
+		// if we are looking at a variable declaration, we need to strip the type declaration and
+		// add a comment over it
+		if (statement.type === 'VariableDeclaration') {
+			transformDeclaration(statement, statement, importSource)
+		}
+
+		// if we are looking at an exported variable, we need to transform the underlying statement
+		if (statement.type === 'ExportNamedDeclaration') {
+			transformDeclaration(statement.declaration, statement, importSource)
+		}
+
+		// the statement is okay to add to the list
+		transformed.push(statement)
+	}
+
+	return (
+		content.substring(0, start) +
+		'\n' +
+		recast.print(AST.program(transformed)).code +
+		'\n' +
+		content.substring(end).trim()
+	)
+}
+
+// transform a variable declaration
+function transformDeclaration(statement, parent, importPaths) {
+	if (
+		statement.type === 'VariableDeclaration' &&
+		statement.declarations[0] &&
+		statement.declarations[0].id.typeAnnotation
+	) {
+		const declaration = statement.declarations[0]
+		// look up the type it was declared as
+		const targetType = declaration.id.typeAnnotation.typeAnnotation.typeName.name
+
+		const source = importPaths[targetType]
+
+		// remove the type annotation
+		declaration.id.typeAnnotation = null
+
+		parent.comments = [AST.commentBlock(` @type { import('${source}').${targetType} } `)]
+	}
+}
+
+export function parseJS(str) {
+	return recast.parse(str || '', {
+		parser: typeScriptParser
+	}).program
 }
---
+++ site/plugins/docs-lang.test.ts
@@ -0,0 +1,50 @@
+import { test, expect } from 'vitest'
+import { transformTypescript } from './docs-lang'
+
+test('change file title', async function () {
+	await expect(
+		transformTypescript(`\`\`\`typescript:title=route/to/typescript.ts
+			console.log('hello')
+        \`\`\``)
+	).resolves.toMatchInlineSnapshot(`
+		"\`\`\`javascript:title=route/to/typescript.js
+		console.log('hello')
+		\`\`\`"
+	`)
+})
+
+test('strip type declarations from variable declarations', async function () {
+	await expect(
+		transformTypescript(`\`\`\`typescript
+		import type { Type } from './source'
+		const Foo: Type = () => {
+			console.log('hello')
+		}
+        \`\`\``)
+	).resolves.toMatchInlineSnapshot(`
+		"\`\`\`javascript
+		/* @type { import('./source').Type } */
+		const Foo = () => {
+		    console.log('hello')
+		};
+		\`\`\`"
+	`)
+})
+
+test('strip type declarations from exported variable declarations', async function () {
+	await expect(
+		transformTypescript(`\`\`\`typescript
+		import type { Type } from './source'
+		export const Foo: Type = () => {
+			console.log('hello')
+		}
+        \`\`\``)
+	).resolves.toMatchInlineSnapshot(`
+		"\`\`\`javascript
+		/* @type { import('./source').Type } */
+		export const Foo = () => {
+		    console.log('hello')
+		};
+		\`\`\`"
+	`)
+})
