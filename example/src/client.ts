a459b6294e244c3e1adf78e0c3b6641dd4daf326
Alec Aivazis <alec@aivazis.com>
2023-01-19 02:18:14 -0800

    subscription handler is now a function of the ClientPlugin context

--- example/src/client.ts
+++ example/src/client.ts
@@ -1,18 +1,14 @@
-import { browser } from '$app/environment'
-import { HoudiniClient, type SubscriptionHandler } from '$houdini'
+import { HoudiniClient } from '$houdini'
 import { subscriptionPlugin } from '$houdini/plugins'
 import { createClient as createWSClient } from 'graphql-ws'
 
-// For subscription (client only)
-let socketClient: SubscriptionHandler | null = null
-if (browser) {
-	// @ts-ignore
-	socketClient = createWSClient({
-		url: 'ws://localhost:4000/graphql',
-	})
-}
-
 export default new HoudiniClient({
 	url: 'http://localhost:4000/graphql',
-	plugins: [subscriptionPlugin(socketClient)],
+	plugins: [
+		subscriptionPlugin(() =>
+			createWSClient({
+				url: 'ws://localhost:4000/graphql',
+			})
+		),
+	],
 })
