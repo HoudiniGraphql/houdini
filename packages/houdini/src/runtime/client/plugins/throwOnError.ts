089b782b0f99b0d0f7ba6258a9e3dfc041c2df49
Alec Aivazis <alec@aivazis.com>
2023-01-20 00:45:52 -0800

    client-plugin review

--- packages/houdini/src/runtime/client/plugins/throwOnError.ts
+++ packages/houdini/src/runtime/client/plugins/throwOnError.ts
@@ -3,7 +3,7 @@ import { ArtifactKind } from '../../lib'
 import type { ClientPlugin } from '../documentStore'
 
 export type ThrowOnErrorParams = {
-	operations: (`all` | 'query' | 'mutation' | 'subscription')[]
+	operations: ('all' | 'query' | 'mutation' | 'subscription')[]
 	error?: (errors: NonNullable<QueryResult<any, any>['errors']>) => unknown
 }
 
