f6ed9308779ff8064d386d39f8ddb7300b930ea4
jycouet <jycouet@gmail.com>
2023-01-20 14:10:47 +0100

    :construction: UPDATE: target to support these api

--- e2e/sveltekit/src/routes/stale/add/+page.svelte
+++ e2e/sveltekit/src/routes/stale/add/+page.svelte
@@ -9,29 +9,57 @@
     }
   `);
 
-  // Option 1
-  const add_all_list_stale = async () => {
+  const markStale_everything = async () => {
     await addUser.mutate(null);
-    cache.markStale('UserNodes');
+    cache.markStale();
   };
 
-  // Option 2
-  const add_node_stale = async () => {
+  const markStale_type = async () => {
     await addUser.mutate(null);
-    cache.markStale('User');
+    cache.markStale({ type: 'UserNodes' });
   };
 
-  // Option 3
-  const add_totalCount = async () => {
+  const markStale_type_field = async () => {
     await addUser.mutate(null);
-    // const userNodes = cache.get('UserNodes'); // data => never. How to do like all?
-    // userNodes.markStale({ field: 'totalCount' });
+    cache.markStale({ type: 'UserNodes', field: 'totalCount' });
+  };
+
+  const markStale_subtype = async () => {
+    await addUser.mutate(null);
+    cache.markStale({ type: 'User' });
+  };
+
+  const markStale_entry = async () => {
+    await addUser.mutate(null);
+    const user = cache.get('User', { id: '1' });
+    user.markStale();
+  };
+
+  const markStale_entry_field = async () => {
+    await addUser.mutate(null);
+    const user = cache.get('User', { id: '1' });
+    user.markStale({ field: 'name' });
+  };
+
+  const markStale_entry_field_when = async () => {
+    await addUser.mutate(null);
+    const user = cache.get('User', { id: '1' });
+    user.markStale({ field: 'name', args: { foo: 'bar' } });
   };
 </script>
 
 <br />
-<button on:click={add_all_list_stale}>add_all_list_stale</button>
+<button on:click={markStale_everything}>markStale_everything</button>
 <br />
-<button on:click={add_node_stale}>add_node_stale</button>
+<button on:click={markStale_type}>markStale_type</button>
 <br />
-<button on:click={add_totalCount}>add_totalCount</button>
+<button on:click={markStale_type_field}>markStale_type_field</button>
+<br />
+<button on:click={markStale_subtype}>markStale_subtype</button>
+<br />
+<button on:click={markStale_entry}>markStale_entry</button>
+<br />
+<button on:click={markStale_entry_field}>markStale_entry_field</button>
+<br />
+<button on:click={markStale_entry_field_when}>markStale_entry_field_when</button>
+<br />
