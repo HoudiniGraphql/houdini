8cec94371c1c7b8b966eedab226d2dabea48488a
Alec Aivazis <alec@aivazis.com>
2023-01-28 13:12:27 -0800

    dry up color config

---
+++ site/src/routes/_diagrams/colors.ts
@@ -0,0 +1,7 @@
+export const colors = {
+	lineColor: ['green', 'var(--contrast)'],
+	canvasColor: ['red', 'var(--hue)'],
+	borderColor: ['orange', 'var(--contrast)'],
+	backgroundColor: ['purple', 'var(--discreet2)'],
+	textColor: ['yellow', 'var(--contrast)']
+}
--- site/src/routes/_diagrams/render.ts
+++ site/src/routes/_diagrams/render.ts
@@ -2,11 +2,7 @@ import { test, expect } from '@playwright/test'
 import { mkdirp } from 'fs-extra'
 import fs from 'fs/promises'
 
-const lineColor = 'var(--contrast)'
-const canvasColor = 'var(--hue)'
-const borderColor = 'var(--contrast)'
-const backgroundColor = 'var(--discreet2)'
-const textColor = 'var(--contrast)'
+import { colors } from './colors.js'
 
 test('has title', async ({ page }) => {
 	await page.goto('http://localhost:3078/_diagrams')
@@ -14,20 +10,17 @@ test('has title', async ({ page }) => {
 	// wait for the diagram to be rendered
 	await page.waitForSelector('pre.mermaid[data-processed="true"]')
 
-	await mkdirp('./src/diagrams')
+	await mkdirp('./src/routes/_diagrams/rendered')
 
 	// loop over the list of diagrams and create the appropriate files
 	const diagrams = ['setup', 'extract', 'validate', 'generate']
 	for (const [i, diagramName] of diagrams.entries()) {
 		// get the diagram contents
-		// and map the internal colors with their semantic equivalent:
-		// green -> line color
-		let fullDiagram = (await page.innerHTML(`pre.mermaid>>nth=${i}`))
-			.replace(/green/g, lineColor)
-			.replace(/red/g, canvasColor)
-			.replace(/orange/g, borderColor)
-			.replace(/purple/, backgroundColor)
-			.replace(/yellow/g, textColor)
+		let fullDiagram = await page.innerHTML(`pre.mermaid>>nth=${i}`)
+		// and map the internal colors with their semantic equivalen
+		for (const [diagramColor, themeColor] of Object.values(colors)) {
+			fullDiagram = fullDiagram.replace(new RegExp(diagramColor, 'g'), themeColor)
+		}
 
 		// we need to add {...$$props} to the tag, so find the first >
 		const firstClose = fullDiagram.indexOf('>')
