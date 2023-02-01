c11d691f1907c54ccb3a9d917b5ef2748431caa8
jycouet <jycouet@gmail.com>
2023-02-01 22:53:20 +0100

    :sparkles: UPDATE: easy path & redirect

---
+++ site/src/hooks.server.js
@@ -0,0 +1,15 @@
+import { redirect } from '@sveltejs/kit'
+
+/** @type {import('@sveltejs/kit').HandleServerError} */
+export function handleError({ error, event }) {
+	const dead_links_redirect_to = {
+		'/api/fragments': '/api/fragment',
+		'/api/vite': '/api/vite-plugin',
+		'/api/cli': '/api/command-line'
+	}
+
+	// If we have a value, let's redirect
+	if (dead_links_redirect_to[event.url.pathname]) {
+		throw redirect(303, dead_links_redirect_to[event.url.pathname])
+	}
+}
