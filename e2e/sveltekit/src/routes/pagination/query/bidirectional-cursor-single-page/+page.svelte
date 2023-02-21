02b381552691587fdb669be2f388e6cd36d92a32
jycouet <jycouet@gmail.com>
2023-02-21 23:19:03 +0100

    :children_crossing: UPDATE: e2e to have pagination without infinite scroll

--- e2e/sveltekit/src/routes/pagination/query/bidirectional-cursor-single-page/+page.svelte
+++ e2e/sveltekit/src/routes/pagination/query/bidirectional-cursor-single-page/+page.svelte
@@ -2,16 +2,29 @@
   import { CachePolicy, graphql } from '$houdini';
 
   $: result = graphql(`
-    query BidirectionalCursorSinglePagePaginationQuery @load {
+    query BidirectionalCursorSinglePagePaginationQuery(
+      $first: Int = 2
+      $after: String = "YXJyYXljb25uZWN0aW9uOjE="
+      $last: Int
+      $before: String
+    ) @load {
       usersConnection(
-        after: "YXJyYXljb25uZWN0aW9uOjE="
-        first: 2
+        first: $first
+        after: $after
+        last: $last
+        before: $before
         snapshot: "pagination-query-bidiriectional-cursor-single-page"
-      ) @paginate {
+      ) {
         edges {
           node {
             name
           }
+        }
+        pageInfo {
+          endCursor
+          hasNextPage
+          hasPreviousPage
+          startCursor
         }
       }
     }
@@ -23,12 +36,40 @@
 </div>
 
 <div id="pageInfo">
-  {JSON.stringify($result.pageInfo)}
+  {JSON.stringify($result.data?.usersConnection.pageInfo)}
 </div>
 
-<button id="previous" on:click={() => result.fetch({ variables: { before: $result.pageInfo.startCursor, last: 2, first: null, after: null } })}>previous</button>
-<button id="next" on:click={() => result.fetch({ variables: { after: $result.pageInfo.endCursor, first: 2, last: null, before: null } })}>next</button>
-
-<button id="refetch" on:click={() => result.fetch({ policy: CachePolicy.NetworkOnly })}
-  >refetch</button
+<button
+  id="previous"
+  on:click={() =>
+    result.fetch({
+      variables: {
+        last: 2,
+        before: $result.data?.usersConnection.pageInfo.startCursor,
+        first: null,
+        after: null
+      }
+    })}
+  disabled={!$result.data?.usersConnection.pageInfo.hasPreviousPage}
 >
+  previous
+</button>
+<button
+  id="next"
+  on:click={() =>
+    result.fetch({
+      variables: {
+        last: null,
+        before: null,
+        after: $result.data?.usersConnection.pageInfo.endCursor,
+        first: 2
+      }
+    })}
+  disabled={!$result.data?.usersConnection.pageInfo.hasNextPage}
+>
+  next
+</button>
+
+<button id="refetch" on:click={() => result.fetch({ policy: CachePolicy.NetworkOnly })}>
+  refetch
+</button>
