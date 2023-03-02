a69a60eb6005fad50c77e8d2120446e19bbfba6a
jycouet <jycouet@gmail.com>
2023-03-02 14:23:03 +0100

    :children_crossing: REMOVE: some parts

--- e2e/svelte/src/App.svelte
+++ e2e/svelte/src/App.svelte
@@ -30,13 +30,7 @@
 		<Counter />
 	</div>
 
-	<p>
-		Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer"
-			>SvelteKit</a
-		>, the official Svelte app framework powered by Vite!
-	</p>
-
-	<p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
+	<p class="read-the-docs">Click on logos to learn more!</p>
 </main>
 
 <style>
