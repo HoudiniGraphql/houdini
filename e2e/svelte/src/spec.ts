2b4107ac077a0461bd3b27da24a19ca9b0cd242e
Alec Aivazis <alec@aivazis.com>
2023-03-10 00:46:20 -0800

    debugging log

--- e2e/svelte/src/spec.ts
+++ e2e/svelte/src/spec.ts
@@ -4,6 +4,9 @@ import { expect, test } from '@playwright/test'
 test('1 query on the home page', async ({ page }) => {
 	await page.goto('/')
 	await sleep(777)
+
+	console.log(await page.innerText('body'))
+
 	const allH2 = await page.getByRole('heading', { level: 2 }).allInnerTexts()
 	expect(allH2).toStrictEqual(['Hello World! // From Houdini!'])
 })
