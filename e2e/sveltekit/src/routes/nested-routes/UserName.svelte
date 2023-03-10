b922c7090c245ec6961a7651a2dc1670e90a2e96
Alec Aivazis <alec@aivazis.com>
2023-03-09 23:51:09 -0800

    grab test

--- e2e/sveltekit/src/routes/nested-routes/UserName.svelte
+++ e2e/sveltekit/src/routes/nested-routes/UserName.svelte
@@ -2,22 +2,22 @@
   import { fragment, graphql, type UserName } from '$houdini';
   import Loading from './Loading.svelte';
 
-  // TODO:
-  // And it can be undefined... because : <UserName user={$GQL_Page_User.data?.user} />
   export let user: UserName | undefined;
 
-  $: frag = fragment(
-    user ?? null,
-    graphql(`
-      fragment UserName on User {
-        name
-      }
-    `)
-  );
+  $: data =
+    user &&
+    fragment(
+      user,
+      graphql(`
+        fragment UserName on User {
+          name
+        }
+      `)
+    );
 </script>
 
-{#if $frag}
-  {$frag.name}
+{#if $data}
+  {$data.name}
 {:else}
   <Loading />
 {/if}
