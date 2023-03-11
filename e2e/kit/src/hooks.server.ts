adbc534e64b7cf55b76cdbcac1fba960a9a18dc3
Alec Aivazis <alec@aivazis.com>
2023-03-10 21:24:19 -0800

    Rename sveltekit e2e directory (#982)

--- e2e/kit/src/hooks.server.ts
+++ e2e/kit/src/hooks.server.ts
@@ -1,10 +1,10 @@
 import { setSession } from '$houdini';
 import type { Handle } from '@sveltejs/kit';
 
-export const handle = (async ({ event, resolve }) => {
+export const handle: Handle = async ({ event, resolve }) => {
   // set the session information for this event
   setSession(event, { user: { token: '1234-Houdini-Token-5678' } });
 
   // pass the event onto the default handle
   return await resolve(event);
-}) satisfies Handle;
+};
