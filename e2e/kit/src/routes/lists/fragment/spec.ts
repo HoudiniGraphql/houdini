1ad2ce9d97734e4991b7a6649105d620e6f0aca0
Alec Aivazis <alec@aivazis.com>
2023-03-14 15:59:42 -0700

    update tests

--- e2e/kit/src/routes/lists/fragment/spec.ts
+++ e2e/kit/src/routes/lists/fragment/spec.ts
@@ -5,5 +5,7 @@ import { goto } from '../../../lib/utils/testsHelper.js';
 test('list fragment', async ({ page }) => {
   await goto(page, routes.Lists_fragment);
   const result = await page.locator('#result').textContent({ timeout: 2997 });
-  expect(result).toMatchSnapshot();
+  expect(result).toEqual(
+    `Bruce Willis - Value1Samuel Jackson - Value1Morgan Freeman - Value1Tom Hanks - Value1Will Smith - Value1Harrison Ford - Value1Eddie Murphy - Value1Clint Eastwood - Value1`
+  );
 });
