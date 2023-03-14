94d5d31b9e310ff12a5adb3027bb3e580d7c0339
Seppe Dekeyser <seppe@dva.be>
2023-03-14 11:12:49 +0100

    Add failing example

---
+++ e2e/kit/src/routes/list-fragment/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import type { PageData } from './$houdini';
+  import UserItem from './UserItem.svelte';
+
+  export let data: PageData;
+  $: ({ ListUsers } = data);
+</script>
+
+{#if $ListUsers.data}
+  <ul>
+    {#each $ListUsers.data.usersConnection.edges as userEdge}
+      {@const user = userEdge.node}
+      {#if user}
+        <UserItem {user} />
+      {/if}
+    {/each}
+  </ul>
+{/if}
---
+++ e2e/kit/src/routes/list-fragment/UserItem.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { fragment, graphql, type UserListItem } from '$houdini';
+
+  export let user: UserListItem;
+  $: data = fragment(
+    user,
+    graphql(`
+      fragment UserListItem on User {
+        id
+        name
+        birthDate
+      }
+    `)
+  );
+</script>
+
+{#if $data}
+  <li>
+    {$data.name} - {$data.birthDate}
+  </li>
+{/if}
