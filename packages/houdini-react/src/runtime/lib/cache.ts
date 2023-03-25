b983e6793c54ff6df22237991e12986f57e0343f
Alec Aivazis <alec@aivazis.com>
2023-03-24 23:57:01 -0700

    add license

--- packages/houdini-react/src/runtime/lib/cache.ts
+++ packages/houdini-react/src/runtime/lib/cache.ts
@@ -1,5 +1,8 @@
-'use strict'
-
+/**
+ * This file is a copy and paste of a very simple and effective LRU cache
+ * using javascript maps. It was copied under the MIT license found at the
+ * bottom of the page.
+ */
 export interface Cache<T> {
 	get(key: string): T | null
 	set(key: string, value: T): void
@@ -74,3 +77,27 @@ class LRUCache<T> implements Cache<T> {
 export function createCache<T>(capacity: number = 1000): LRUCache<T> {
 	return new LRUCache<T>(capacity)
 }
+
+/**
+MIT License
+
+Copyright (c) Meta Platforms, Inc. and affiliates.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
