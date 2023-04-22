606db2afe0f6fa57d2827871e0e3945267288cfb
Alec Aivazis <alec@aivazis.com>
2023-04-22 13:51:52 -0700

    reproduce in unit tests

--- e2e/kit/src/routes/abstract-fragments/nested-connection/spec.ts
+++
@@ -1,9 +0,0 @@
-import { test } from '@playwright/test';
-import { routes } from '../../../lib/utils/routes.js';
-import { expect_to_be } from '../../../lib/utils/testsHelper.js';
-
-test('abstract fragment with nested connection', async ({ page }) => {
-  await page.goto(routes.abstractFragments_nestedConnection);
-
-  await expect_to_be(page, 'Terk  King Louie');
-});
