b6497844268ed6688f1e7b9a7848c3fab8194523
Alec Aivazis <alec@aivazis.com>
2023-05-14 17:40:20 -0700

    React Link prop type fixes (#1088)

--- packages/houdini-react/src/runtime/routing/lib/is_pending.tsx
+++ packages/houdini-react/src/runtime/routing/lib/is_pending.tsx
@@ -1,3 +1,5 @@
-export function isPending(value: any): value is symbol {
+import { PendingValue } from '$houdini/runtime/lib/types'
+
+export function isPending(value: any): value is typeof PendingValue {
 	return typeof value === 'symbol'
 }
