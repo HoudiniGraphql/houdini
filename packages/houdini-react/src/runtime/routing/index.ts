1e3a0cd4491c4055a5172f36df967b471ddd860e
Alec Aivazis <alec@aivazis.com>
2023-05-17 22:27:01 -0700

    move isPending to houdini core

--- packages/houdini-react/src/runtime/routing/index.ts
+++ packages/houdini-react/src/runtime/routing/index.ts
@@ -1,4 +1,3 @@
 export * from './components'
 export { suspense_cache } from './lib/cache'
 export type { RouterManifest, RouterPageManifest } from './lib/types'
-export { isPending } from './lib/is_pending'
