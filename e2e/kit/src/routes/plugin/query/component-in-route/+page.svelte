447469a1afc749cce7c12df19ffe1f0707794da1
Seppe Dekeyser <seppe@dva.be>
2023-07-17 21:16:53 +0200

    Add reproduction
    Notice that the typings for ComponentInRoute_ComponentVariables are wonky

---
+++ e2e/kit/src/routes/plugin/query/component-in-route/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import type { PageData } from './$houdini';
+  import UserDetails from './UserDetails.svelte';
+
+  export let data: PageData;
+  $: ({ ComponentInRoute_Route } = data);
+</script>
+
+{#if $ComponentInRoute_Route.data}
+  <ul>
+    {#each $ComponentInRoute_Route.data.usersList as user}
+      <UserDetails userId={user.id} />
+    {/each}
+  </ul>
+{/if}
