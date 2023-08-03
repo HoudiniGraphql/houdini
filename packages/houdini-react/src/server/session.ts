22b20557550f190f01f6bd29a6b484704837292f
jycouet <jycouet@gmail.com>
2023-08-03 13:53:47 +0200

    :bug: FIX: if cookie not found don't return session

--- packages/houdini-react/src/server/session.ts
+++ packages/houdini-react/src/server/session.ts
@@ -31,7 +31,14 @@ export function get_session(req: Headers, secrets: string[]): App.Session {
 		return {}
 	}
 
-	// TODO: Alec & JYC (I had a lot of cookie, & none from Houdini, this is crashing. we should check parsed.split('=')[0] to have a certain key?)
-	// the value is an object with session information
-	return JSON.parse(parsed.split('=')[1])
+	const houdini_session_cookie = parsed
+		.split(';')
+		.map((s) => s.trim().split('='))
+		.filter((s) => s[0] === session_cookie_name)
+
+	if (houdini_session_cookie.length === 1) {
+		return JSON.parse(houdini_session_cookie[0][1])
+	}
+
+	return {}
 }
