8e7c1bbae7ad504b95897c0df6da8865b506fb4a
jycouet <jycouet@gmail.com>
2023-08-22 15:34:22 +0200

    :ok_hand: FIX: eslint warning

--- e2e/kit/src/routes/plugin/query/component-in-route/spec.ts
+++ e2e/kit/src/routes/plugin/query/component-in-route/spec.ts
@@ -6,7 +6,7 @@ test.describe('query preprocessor', () => {
   test('component queries inside a route', async ({ page }) => {
     await goto_expect_n_gql(page, routes.Plugin_query_componentInRoute, 2);
 
-    await expect_to_be(page, 'Bruce Willis', 'li[id=result-0]');
-    await expect_to_be(page, 'Samuel Jackson', 'li[id=result-1]');
+    await expect_to_be(page, 'Bruce Willis - (1)', 'li[id=result-0]');
+    await expect_to_be(page, 'Samuel Jackson - (2)', 'li[id=result-1]');
   });
 });
