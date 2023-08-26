d08ed3762424c5ab46ed9fb743a29b45b1f3ea2b
Alec Aivazis <alec@aivazis.com>
2023-08-25 18:40:32 -0700

    copy template source

---
+++ packages/create-houdini/README.md
@@ -0,0 +1,9 @@
+# create-houdini
+
+A CLI for creating new [Houdini](https://houdinigraphql.com/) projects. Just run...
+
+```bash
+npm create houdini@latest
+```
+
+...and follow the prompts.
---
+++ packages/create-houdini/bin.js
@@ -0,0 +1,116 @@
+#!/usr/bin/env node
+import * as p from '@clack/prompts'
+import { execSync } from 'child_process'
+import { green, grey } from 'kleur/colors'
+import fs from 'node:fs'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+// load the version of create-houdini that this was run with by looking at the packge's package.json
+const { version } = JSON.parse(fs.readFileSync(new URL('package.json', import.meta.url), 'utf-8'))
+
+// the first argument is the name of the project
+let projectDir = process.argv[2]
+let projectName = projectDir
+
+console.log(`${grey(`create-houdini version ${version}`)}
+`)
+
+p.intro('🎩 Welcome to Houdini!')
+
+// if we weren't given a directory, then we should ask
+if (!projectDir) {
+	const dir = await p.text({
+		message: `Where should we create your project?`,
+		placeholder: '  (press Enter to use the current directory)',
+	})
+
+	if (p.isCancel(dir)) {
+		process.exit(1)
+	}
+
+	if (dir) {
+		projectDir = dir
+		projectName = 'hello-houdini'
+	} else {
+		projectDir = '.'
+	}
+}
+
+// if we were told to use the current directory then we need
+// a more appropriate name for the project
+if (projectDir === '.') {
+	projectName = 'hello-houdini'
+}
+
+// project location emtpy?
+if (fs.existsSync(projectDir)) {
+	if (fs.readdirSync(projectDir).length > 0) {
+		const force = await p.confirm({
+			message:
+				'Target directory is not empty. Continue anyway? This might overwrite existing files.',
+			initialValue: false,
+		})
+
+		// bail if `force` is `false` or the user cancelled with Ctrl-C
+		if (force !== true) {
+			process.exit(1)
+		}
+	}
+}
+
+const template = await p.select({
+	message: 'Which template do you want to use?',
+	initialValue: 'react-typescript',
+	options: [
+		{
+			label: 'React w/ TypeScript',
+			value: 'react-typescript',
+		},
+	],
+})
+const templateDir = sourcePath(`./templates/${template}`)
+
+copy(templateDir, projectDir, {})
+
+p.outro('🎉 Everything is ready!')
+
+// .) add some default files (to have a basic layout)
+// Function to copy files recursively
+function copy(
+	/** @type {string} */ sourceDir,
+	/** @type {string} */ destDir,
+	/** @type {Record<string, (val: string) => string>} */ transformMap
+) {
+	if (!fs.existsSync(destDir)) {
+		fs.mkdirSync(destDir)
+	}
+
+	const files = fs.readdirSync(sourceDir)
+	for (const file of files) {
+		const sourceFilePath = path.join(sourceDir, file)
+		const destFilePath = path.join(destDir, file)
+		const sourceRelative = path.relative(templateDir, sourceFilePath)
+
+		const stats = fs.statSync(sourceFilePath)
+
+		// files need to be copied and potentially transformed
+		if (stats.isFile()) {
+			// read the source file
+			const source = fs.readFileSync(sourceFilePath)
+
+			// apply any transformations if necessary
+			const transformed = transformMap[sourceRelative]?.(source.toString()) ?? source
+			// write the result
+			fs.writeFileSync(destFilePath, transformed)
+		}
+		// if we run into a directory then we should keep going
+		else if (stats.isDirectory()) {
+			copy(sourceFilePath, destFilePath, transformMap)
+		}
+	}
+}
+
+export function sourcePath(/** @type {string} */ path) {
+	return fileURLToPath(new URL(path, import.meta.url).href)
+}
