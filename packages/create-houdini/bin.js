a521388e66515651804ff863e74669936190a295
jycouet <jycouet@gmail.com>
2023-08-28 09:41:46 +0200

    add .meta for options per template & pullSchemaCli

--- packages/create-houdini/bin.js
+++ packages/create-houdini/bin.js
@@ -1,15 +1,14 @@
 #!/usr/bin/env node
 import * as p from '@clack/prompts'
 import * as graphql from 'graphql'
-import { green, grey } from 'kleur/colors'
-import fs from 'node:fs'
+import { grey } from 'kleur/colors'
+import fs, { readFileSync, writeFileSync } from 'node:fs'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 
 // the first argument is the name of the project
 let projectDir = process.argv[2]
 let projectName = projectDir
-let apiUrl = ''
 
 const schemaPath = './schema.graphql'
 
@@ -43,6 +42,7 @@ if (!projectDir) {
 if (projectDir === '.') {
 	projectName = 'hello-houdini'
 }
+let dirToCreate = true
 
 // project location emtpy?
 if (fs.existsSync(projectDir)) {
@@ -52,57 +52,72 @@ if (fs.existsSync(projectDir)) {
 				'Target directory is not empty. Continue anyway? This might overwrite existing files.',
 			initialValue: false,
 		})
+		dirToCreate = false
 
 		// bail if `force` is `false` or the user cancelled with Ctrl-C
 		if (force !== true) {
 			process.exit(1)
 		}
 	}
-} else {
-	// create the directory
-	fs.mkdirSync(projectDir)
 }
+
+const templatesDir = sourcePath(`./templates`)
+const options = fs.readdirSync(templatesDir).map((template) => {
+	const metaPath = path.join(templatesDir, template, '.meta.json')
+	if (!fs.existsSync(metaPath)) {
+		p.cancel(
+			`❌ Template "${template}" is missing a ".meta.json" file to describe options (value!, label, hint, apiUrl)`
+		)
+		process.exit(1)
+	}
+	return JSON.parse(readFileSync(metaPath, 'utf-8'))
+})
 
 const template = await p.select({
 	message: 'Which template do you want to use?',
 	initialValue: 'react-typescript',
-	options: [
-		{
-			label: 'React',
-			value: 'react',
-		},
-		{
-			label: 'React w/ TypeScript',
-			value: 'react-typescript',
-		},
-	],
+	options,
 })
 if (p.isCancel(template)) {
 	process.exit(1)
 }
+const templateMeta = options.find((option) => option.value === template)
 
-const templateDir = sourcePath(`./templates/${template}`)
+let apiUrl = templateMeta.apiUrl ?? ''
+if (apiUrl === '') {
+	apiUrl = await pullSchemaCli()
+}
 
-await pullSchema()
+const templateDir = sourcePath(path.join(templatesDir, template))
+// create the project directory if necessary
+if (dirToCreate) {
+	fs.mkdirSync(projectDir)
+}
 
-copy(templateDir, projectDir, {
-	API_URL: apiUrl,
-	PROJECT_NAME: projectName,
-	HOUDINI_VERSION: version,
-})
+copy(
+	templateDir,
+	projectDir,
+	{
+		API_URL: apiUrl,
+		PROJECT_NAME: projectName,
+		HOUDINI_VERSION: version,
+	},
+	['.meta.json']
+)
 
 p.outro(`🎉 Everything is ready!
 
 👉 Next Steps
-1️⃣  Install dependencies:       npm i | pnpm i | yarn
-2️⃣  Start your application:     npm run dev | pnpm run dev | yarn dev
+1️⃣  Install dependencies   :  npm i       | pnpm i   | yarn
+2️⃣  Start your application :  npm run dev | pnpm dev | yarn dev
 `)
 
 // Function to copy files recursively
 function copy(
 	/** @type {string} */ sourceDir,
 	/** @type {string} */ destDir,
-	/** @type {Record<string, string>} */ transformMap
+	/** @type {Record<string, string>} */ transformMap,
+	/** @type {string[]} */ ignoreList
 ) {
 	if (!fs.existsSync(destDir)) {
 		fs.mkdirSync(destDir)
@@ -111,27 +126,33 @@ function copy(
 	const files = fs.readdirSync(sourceDir)
 	for (const file of files) {
 		const sourceFilePath = path.join(sourceDir, file)
-		const destFilePath = path.join(destDir, file)
 		const sourceRelative = path.relative(templateDir, sourceFilePath)
+		// skip the ignore list
+		if (!ignoreList.includes(sourceRelative)) {
+			const destFilePath = path.join(destDir, file)
 
-		const stats = fs.statSync(sourceFilePath)
+			const stats = fs.statSync(sourceFilePath)
 
-		// files need to be copied and potentially transformed
-		if (stats.isFile()) {
-			// read the source file
-			const source = fs.readFileSync(sourceFilePath)
+			// files need to be copied and potentially transformed
+			if (stats.isFile()) {
+				// read the source file
+				const source = fs.readFileSync(sourceFilePath)
 
-			// apply any transformations if necessary
-			const transformed = Object.entries(transformMap).reduce((prev, [pattern, value]) => {
-				return prev.replaceAll(pattern, value)
-			}, source.toString())
+				// apply any transformations if necessary
+				const transformed = Object.entries(transformMap).reduce(
+					(prev, [pattern, value]) => {
+						return prev.replaceAll(pattern, value)
+					},
+					source.toString()
+				)
 
-			// write the result
-			fs.writeFileSync(destFilePath, transformed)
-		}
-		// if we run into a directory then we should keep going
-		else if (stats.isDirectory()) {
-			copy(sourceFilePath, destFilePath, transformMap)
+				// write the result
+				fs.writeFileSync(destFilePath, transformed)
+			}
+			// if we run into a directory then we should keep going
+			else if (stats.isDirectory()) {
+				copy(sourceFilePath, destFilePath, transformMap, ignoreList)
+			}
 		}
 	}
 }
@@ -140,45 +161,119 @@ function sourcePath(/** @type {string} */ path) {
 	return fileURLToPath(new URL(path, import.meta.url).href)
 }
 
-async function pullSchema() {
-	try {
-		apiUrl = await p.text({
-			message: "What's the URL for your api?",
-			placeholder: 'http://localhost:4000/graphql',
-			defaultValue: 'http://localhost:4000/graphql',
-		})
+async function pullSchemaCli() {
+	let number_of_round = 0
+	let url_and_headers = ''
+	let apiUrl = ''
+	let pullSchema_content = null
+	while (pullSchema_content === null && number_of_round < 10) {
+		number_of_round++
+		const answer = await p.group(
+			{
+				url_and_headers: async () =>
+					p.text({
+						message: `What's the URL for your api? ${
+							number_of_round === 1 ? '' : `(attempt ${number_of_round})`
+						}`,
+						placeholder: `http://localhost:4000/graphql ${
+							number_of_round === 1 ? '' : 'Authorization=Bearer MyToken'
+						}`,
+						// initialValue: url_and_headers,
+						validate: (value) => {
+							// If empty, let's assume the placeholder value
+							if (value === '') {
+								return
+							}
 
-		if (p.isCancel(apiUrl)) {
-			process.exit(1)
-		}
-
-		// verify we can send graphql queries to the server
-		const response = await fetch(apiUrl, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
+							if (!value.startsWith('http')) {
+								return 'Please enter a valid URL'
+							}
+						},
+					}),
 			},
+			{
+				onCancel: () => pCancel(),
+			}
+		)
+
+		url_and_headers = answer.url_and_headers
+		const value_splited = url_and_headers.split(' ')
+		const apiUrl = value_splited[0]
+
+		const local_headers =
+			value_splited.length > 1
+				? // remove the url and app all the headers
+				  extractHeadersStr(value_splited.slice(1).join(' '))
+				: {}
+
+		pullSchema_content = await pullSchema(apiUrl, schemaPath, local_headers)
+
+		if (pullSchema_content === null) {
+			const msg = `If you need to pass headers, add them after the URL (eg: '${`${apiUrl} Authorization=Bearer MyToken`}')`
+			p.log.error(msg)
+		}
+	}
+
+	// if we are here... it means that we have tried x times to pull the schema and it failed
+	if (pullSchema_content === null) {
+		pCancel("We couldn't pull the schema. Please check your URL/headers and try again.")
+	}
+
+	return apiUrl
+}
+
+async function pullSchema(
+	/** @type {string} */ url,
+	/** @type {string} */ schemaPath,
+	/** @type {Record<string, string>} */ headers
+) {
+	let content = ''
+	try {
+		// send the request
+		const resp = await fetch(url, {
+			method: 'POST',
 			body: JSON.stringify({
 				query: graphql.getIntrospectionQuery(),
 			}),
+			headers: { 'Content-Type': 'application/json', ...headers },
 		})
+		content = await resp.text()
 
-		// if the response was not a 200, we have a problem
-		if (response.status !== 200) {
-			console.log('❌ That URL is not accepting GraphQL queries. Please try again.')
-			return await pullSchema()
-		}
-
-		// make sure we can parse the response as json
-		const content = await response.text()
 		const jsonSchema = JSON.parse(content).data
-		const schema = graphql.buildClientSchema(jsonSchema)
+		let fileData = ''
 
-		// write the schema to disk
-		await fs.writeFileSync(path.join(projectDir, schemaPath), JSON.stringify(jsonSchema))
-	} catch (e) {
-		console.log('❌ Something went wrong: ' + e.message)
-		console.log(e)
-		return await pullSchema()
+		fileData = JSON.stringify(jsonSchema)
+
+		await writeFileSync(schemaPath, fileData)
+
+		return fileData
+	} catch (/** @type {any} */ e) {
+		if (content) {
+			console.warn(
+				`⚠️  Couldn't pull your schema.
+${'   Reponse:'} ${content}
+${'   Error  :'} ${e.message}`
+			)
+		} else {
+			console.warn(`⚠️  Couldn't pull your schema: ${e.message}`)
+		}
 	}
+	return null
+}
+
+function extractHeadersStr(/** @type {string} */ str) {
+	const regex = /(\w+)=("[^"]*"|[^ ]*)/g
+	const /** @type {Record<string, string>} */ obj = {}
+
+	let match
+	while ((match = regex.exec(str ?? '')) !== null) {
+		obj[match[1]] = match[2].replaceAll('"', '')
+	}
+
+	return obj
+}
+
+function pCancel(cancelText = 'Operation cancelled.') {
+	p.cancel(cancelText)
+	process.exit(1)
 }
