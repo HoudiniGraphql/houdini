f46a2256ddf4485b5bb1453bab368902afdeab48
jycouet <jycouet@gmail.com>
2023-08-28 23:38:33 +0200

    tmp avoid the tab issue!

--- packages/create-houdini/templates/react-typescript/src/routes/+page.tsx
+++ packages/create-houdini/templates/react-typescript/src/routes/+page.tsx
@@ -7,8 +7,7 @@ export default function () {
 			<h2>Home</h2>
 
 			<p>
-				Visit <a href="https://houdinigraphql.com/">Houdini Graphql</a> to read the
-				documentation.
+				Visit <a href="https://houdinigraphql.com/">Houdini Graphql</a> to read the doc.
 			</p>
 		</div>
 	)
