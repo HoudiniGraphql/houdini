7bd084048d9cf7b82d31211292eacdfac1d9e33f
jycouet <jycouet@gmail.com>
2023-10-05 23:39:20 +0200

    using helpers

--- e2e/react/src/routes/spec.ts
+++ e2e/react/src/routes/spec.ts
@@ -1,14 +1,10 @@
 import { test } from '@playwright/test'
-import { expect } from '@playwright/test'
 
-// import { routes } from '../utils/routes'
-// import { expect_to_be } from '../utils/testsHelper'
+import { routes } from '../utils/routes.js'
+import { expect_to_be } from '../utils/testsHelper.js'
 
 test('Integration has the right title, we can start 🚀', async ({ page }) => {
-	// await page.goto(routes.Home)
-	await page.goto('/')
+	await page.goto(routes.Home)
 
-	// await expect_to_be(page, "Houdini's SvelteKit Interation tests", 'h1')
-	const result = await page.locator('h1').textContent({ timeout: 2998 })
-	expect(result).toBe("Houdini's React Interation tests")
+	await expect_to_be(page, "Houdini's React Interation tests", 'h1')
 })
