882ebf3d37a76bdde6f41797c341866e3256be1a
Alec Aivazis <alec@aivazis.com>
2023-10-14 19:41:21 -0700

    specify preview port from playwright

--- e2e/react/src/routes/features/component_fields/simple/spec.ts
+++ e2e/react/src/routes/features/component_fields/simple/spec.ts
@@ -2,13 +2,13 @@ import { expect, test } from '@playwright/test'
 import { goto } from '~/utils/testsHelper.js'
 
 test('Component field happy path', async ({ page }) => {
-	await goto(page, '/features/component_fields/arguments')
+	await goto(page, '/features/component_fields/simple')
 
 	// find all of the images
 	const images = await page.locator('img').all()
 	// every image should have a size of 50
 	for (const image of images) {
 		expect(await image.getAttribute('height')).toBe('30')
-		expect(await image.getAttribute('src')).toContain('size=30')
+		expect(await image.getAttribute('src')).not.toContain('size=')
 	}
 })
