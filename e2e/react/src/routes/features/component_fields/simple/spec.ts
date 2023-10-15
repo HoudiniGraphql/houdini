71bb315343e54f1abcd97bcb09dfc006bcd3bc7c
Alec Aivazis <alec@aivazis.com>
2023-10-14 19:37:30 -0700

    update tests

--- e2e/react/src/routes/features/component_fields/simple/spec.ts
+++ e2e/react/src/routes/features/component_fields/simple/spec.ts
@@ -1,14 +1,14 @@
 import { expect, test } from '@playwright/test'
 import { goto } from '~/utils/testsHelper.js'
 
-test('Component fields with correct argument value', async ({ page }) => {
+test('Component field happy path', async ({ page }) => {
 	await goto(page, '/features/component_fields/arguments')
 
 	// find all of the images
 	const images = await page.locator('img').all()
 	// every image should have a size of 50
 	for (const image of images) {
-		expect(await image.getAttribute('height')).toBe('50')
-		expect(await image.getAttribute('src')).toContain('size=50')
+		expect(await image.getAttribute('height')).toBe('30')
+		expect(await image.getAttribute('src')).toContain('size=30')
 	}
 })
