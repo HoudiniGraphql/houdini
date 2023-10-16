3b676a0280a05fea35c73d99f8b54289255a7ff4
Alec Aivazis <alec@aivazis.com>
2023-10-16 03:41:30 -0700

    fix out of order instructinos

--- packages/houdini-react/src/runtime/routing/Router.tsx
+++ packages/houdini-react/src/runtime/routing/Router.tsx
@@ -199,20 +199,6 @@ function usePageData({
 								const artifactName = "${artifact.name}"
 								const value = ${JSON.stringify(observer.state.data)}
 
-								// if the data is pending, we need to resolve it
-								if (window.__houdini__nav_caches__?.data_cache.has(artifactName)) {
-									// before we resolve the pending signals,
-									// fill the data cache with values we got on the server
-									const new_store = window.__houdini__client__.observe({
-										artifact: window.__houdini__nav_caches__.artifact_cache.get(artifactName),
-										cache: window.__houdini__cache__,
-										initialValue: value,
-									})
-
-									window.__houdini__nav_caches__?.data_cache.set(artifactName, new_store)
-								}
-
-
 								// if there are no data caches available we need to populate the pending one instead
 								if (!window.__houdini__nav_caches__) {
 									if (!window.__houdini__pending_data__) {
@@ -232,16 +218,29 @@ function usePageData({
 									window.__houdini__pending_artifacts__[artifactName] = ${JSON.stringify(artifact)}
 								}
 
-								if (window.__houdini__nav_caches__?.pending_cache.has(artifactName)) {
-									// we're pushing this store onto the client, it should be initialized
-									new_store.send({
-										setup: true,
-										variables: ${JSON.stringify(variables)}
+								// if the data is pending, we need to resolve it
+								if (window.__houdini__nav_caches__?.data_cache.has(artifactName)) {
+									// before we resolve the pending signals,
+									// fill the data cache with values we got on the server
+									const new_store = window.__houdini__client__.observe({
+										artifact: window.__houdini__nav_caches__.artifact_cache.get(artifactName),
+										cache: window.__houdini__cache__,
+										initialValue: value,
 									})
 
-									// notify anyone waiting on the pending cache
-									window.__houdini__nav_caches__.pending_cache.get(artifactName).resolve()
-									window.__houdini__nav_caches__.pending_cache.delete(artifactName)
+									window.__houdini__nav_caches__?.data_cache.set(artifactName, new_store)
+
+									if (window.__houdini__nav_caches__?.pending_cache.has(artifactName)) {
+										// we're pushing this store onto the client, it should be initialized
+										new_store.send({
+											setup: true,
+											variables: ${JSON.stringify(variables)}
+										})
+
+										// notify anyone waiting on the pending cache
+										window.__houdini__nav_caches__.pending_cache.get(artifactName).resolve()
+										window.__houdini__nav_caches__.pending_cache.delete(artifactName)
+									}
 								}
 							}
 						</script>
