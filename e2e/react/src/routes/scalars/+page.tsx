74e17a6ccf8ce3b20e5c52651f197657e0dab6ff
Alec Aivazis <alec@aivazis.com>
2024-03-07 22:23:19 -0800

    ignore fragmentKey when marshaling

--- e2e/react/src/routes/scalars/+page.tsx
+++ e2e/react/src/routes/scalars/+page.tsx
@@ -1,9 +1,13 @@
 import { PageProps } from './$types'
 
 export default ({ scalar_test }: PageProps) => {
-	return scalar_test.usersList.map((user) => (
-		<div key={user.name}>
-			{user.name} - {user.birthDate?.toLocaleDateString()}
+	return (
+		<div id="result">
+			{scalar_test.usersList.map((user) => (
+				<div key={user.name}>
+					{user.name} - {user.birthDate?.toLocaleDateString()}
+				</div>
+			))}
 		</div>
-	))
+	)
 }
