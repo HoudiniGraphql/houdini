ff8dbd4b0062e9b5ef6d327f27f54f7a987e25ad
Alec Aivazis <alec@aivazis.com>
2024-03-07 22:15:20 -0800

    better formatting for scalar test

--- e2e/react/src/routes/scalars/+page.tsx
+++ e2e/react/src/routes/scalars/+page.tsx
@@ -1,5 +1,9 @@
 import { PageProps } from './$types'
 
 export default ({ scalar_test }: PageProps) => {
-	return scalar_test.usersList.map((user) => user.birthDate?.toISOString?.())
+	return scalar_test.usersList.map((user) => (
+		<div key={user.name}>
+			{user.name} - {user.birthDate?.toLocaleDateString()}
+		</div>
+	))
 }
