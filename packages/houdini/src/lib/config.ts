70dab292b4590183f7c0ccfa3731c28c4ea3f6b1
Gwen Le Bihan <hey@ewen.works>
2024-11-13 01:56:38 +0100

    Don't crash dev server when a schema loading error occurs (#1384)

--- packages/houdini/src/lib/config.ts
+++ packages/houdini/src/lib/config.ts
@@ -1173,7 +1173,11 @@ export async function getConfig({
 
 			// the schema is safe to load
 			if (schemaOk && !noSchema) {
-				_config.schema = await loadSchemaFile(_config.schemaPath)
+				try {
+					_config.schema = await loadSchemaFile(_config.schemaPath)
+				} catch (e) {
+					console.error(`⚠️  Your schema file could not be loaded: ${e}`)
+				}
 			}
 		}
 
