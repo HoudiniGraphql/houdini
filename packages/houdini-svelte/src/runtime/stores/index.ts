b3701e6ac9c218d06b8b78539f64fd5c7bbbe610
Alec Aivazis <alec@aivazis.com>
2025-03-25 17:22:45 -0700

    reorder exports

--- packages/houdini-svelte/src/runtime/stores/index.ts
+++ packages/houdini-svelte/src/runtime/stores/index.ts
@@ -1,4 +1,4 @@
+export { QueryStore, QueryStoreCursor, QueryStoreOffset } from './query'
 export { FragmentStore, FragmentStoreCursor, FragmentStoreOffset } from './fragment'
 export { SubscriptionStore } from './subscription'
 export { MutationStore, type MutationConfig } from './mutation'
-export { QueryStore, QueryStoreCursor, QueryStoreOffset } from './query'
