afc2b2acc3e167bdfe4be1121b3d5d64c0b6674e
Alec Aivazis <alec@aivazis.com>
2025-03-26 01:01:20 -0700

    patch init

--- packages/houdini-svelte/src/runtime/client.ts
+++ packages/houdini-svelte/src/runtime/client.ts
@@ -10,6 +10,18 @@ export async function initClient(): Promise<HoudiniClient> {
 
 	// @ts-ignore
 	client = (await import('HOUDINI_CLIENT_PATH')).default
+
+  // the import might not work immediately during hmr so patch init to ensure we wait until we get something
+  const delay = (ms:number) => new Promise(res => setTimeout(res, ms))
+  for (let retry = 0; retry < 10; retry++) {
+    if (client) {
+      break
+    }
+
+    await delay(100)
+    // @ts-ignore
+	  client = (await import('HOUDINI_CLIENT_PATH')).default
+  }
 	return client!
 }
 
