fcdbfbe2ca6ed7f31a897de9a6c67781b0dfdfdf
Alec Aivazis <alec@aivazis.com>
2025-04-25 01:53:45 -0700

    generate and include static runtimes before collecting documents

--- packages/houdini/src/codegen/generators/runtime/pluginRuntime.ts
+++ packages/houdini/src/codegen/generators/runtime/pluginRuntime.ts
@@ -27,6 +27,41 @@ export function moduleStatments(config: Config) {
 	}
 }
 
+export async function generateStaticRuntimes({ config }: { config: Config }) {
+	if (houdini_mode.is_testing) {
+		return
+	}
+
+	// generate the runtime for each plugin
+	await Promise.all(
+		config.plugins
+			.filter((plugin) => plugin.staticRuntime)
+			.map(async (plugin) => {
+				// a plugin has told us to include a runtime then the path is relative to the plugin file
+				const runtime_path = config.pluginRuntimeSource(plugin)
+				if (!runtime_path) {
+					return
+				}
+
+				// make sure the source file exists
+				try {
+					await fs.stat(runtime_path)
+				} catch {
+					throw new HoudiniError({
+						message: 'Cannot find runtime to generate for ' + plugin.name,
+						description: 'Maybe it was bundled?',
+					})
+				}
+
+				// copy the runtime
+				const pluginDir = config.pluginStaticRuntimeDirectory(plugin.name)
+
+				await fs.mkdirp(pluginDir)
+				await fs.recursiveCopy(runtime_path, pluginDir)
+			})
+	)
+}
+
 export async function generatePluginRuntimes({
 	config,
 	docs,
