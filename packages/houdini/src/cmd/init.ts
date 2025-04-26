6a59dbe745143cd78d7e58585c8655e7252e49c9
Gonzalo Ruiz <rgon@rgon.es>
2025-04-26 12:12:14 +0200

    refactor(cmd/init): deduplicate writing pulled schema with same method in pullSchema()

--- packages/houdini/src/cmd/init.ts
+++ packages/houdini/src/cmd/init.ts
@@ -157,7 +157,7 @@ export async function init(
 				fetchTimeout,
 				schemaPath,
 				local_headers,
-				false
+				true
 			)
 
 			if (pullSchema_content === null) {
@@ -196,11 +196,6 @@ export async function init(
 		)
 
 		schemaPath = answers.schema_path
-	}
-
-	// Let's write the schema only now (after the function "after_questions" where the project has been created)
-	if (is_remote_endpoint && pullSchema_content) {
-		await fs.writeFile(path.join(targetPath, schemaPath), pullSchema_content)
 	}
 
 	// try to detect which tools they are using
