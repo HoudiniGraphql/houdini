bcb8d38a7ab729594bbc69058ff4ab15eb5f44b1
Alec Aivazis <alec@aivazis.com>
2025-05-10 01:26:59 -0700

    more tests

--- packages/houdini-core/plugin/documents/artifacts/selection_operations_test.go
+++ packages/houdini-core/plugin/documents/artifacts/selection_operations_test.go
@@ -361,6 +361,133 @@ func TestArtifactOperationsGeneration(t *testing.T) {
 				},
 			},
 			{
+				Name: "Insert operations allList and @with directive",
+				Pass: true,
+				Input: []string{
+					`mutation A {
+              addFriend {
+                friend {
+                    ...All_Users_insert @with(filter: "Hello World") @allLists
+                }
+              }
+            }`,
+					`query TestQuery($filter: String) {
+              users @list(name: "All_Users") {
+                firstName
+                field(filter: $filter)
+              }
+            }`,
+				},
+				Extra: map[string]any{
+					"A": tests.Dedent(`
+              export default {
+                  "name": "A",
+                  "kind": "HoudiniMutation",
+                  "hash": "478267e6079162675775c31eaffa1e1108c883b24f7b3ff81f1caed9ad415cd6",
+                  "raw": ` + "`" + `mutation A {
+                  addFriend {
+                      friend {
+                          ...All_Users_insert_kVR6H
+                          __typename
+                          id
+                      }
+                      __typename
+                  }
+              }
+
+              fragment All_Users_insert_kVR6H on User {
+                  firstName
+                  __typename
+                  id
+                  field(filter: "Hello World")
+              }
+              ` + "`" + `,
+
+                  "rootType": "Mutation",
+                  "stripVariables": [],
+
+                  "selection": {
+                      "fields": {
+                          "addFriend": {
+                              "type": "AddFriendOutput",
+                              "keyRaw": "addFriend",
+
+                              "selection": {
+                                  "fields": {
+                                      "__typename": {
+                                          "type": "String",
+                                          "keyRaw": "__typename",
+                                          "visible": true,
+                                      },
+
+                                      "friend": {
+                                          "type": "User",
+                                          "keyRaw": "friend",
+
+                                          "operations": [{
+                                              "action": "insert",
+                                              "list": "All_Users",
+                                              "position": "last",
+                                              "target": "all"
+                                          }],
+
+                                          "selection": {
+                                              "fields": {
+                                                  "__typename": {
+                                                      "type": "String",
+                                                      "keyRaw": "__typename",
+                                                      "visible": true,
+                                                  },
+
+                                                  "field": {
+                                                      "type": "String",
+                                                      "keyRaw": "field(filter: \"Hello World\")",
+                                                      "nullable": true,
+                                                  },
+
+                                                  "firstName": {
+                                                      "type": "String",
+                                                      "keyRaw": "firstName",
+                                                  },
+
+                                                  "id": {
+                                                      "type": "ID",
+                                                      "keyRaw": "id",
+                                                      "visible": true,
+                                                  },
+                                              },
+
+                                              "fragments": {
+                                                  "All_Users_insert": {
+                                                      "arguments": {
+                                                          "filter": {
+                                                              "kind": "StringValue",
+                                                              "value": "Hello World"
+                                                          },
+                                                      }
+                                                  },
+                                              },
+                                          },
+
+                                          "visible": true,
+                                      },
+                                  },
+                              },
+
+                              "visible": true,
+                          },
+                      },
+                  },
+
+                  "pluginData": {},
+              }
+
+              "HoudiniHash=478267e6079162675775c31eaffa1e1108c883b24f7b3ff81f1caed9ad415cd6"
+
+          `),
+				},
+			},
+			{
 				Name: "Optimistic keys",
 				Pass: true,
 				Input: []string{
