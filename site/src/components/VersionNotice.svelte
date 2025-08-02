c43a1bf4fd907e17ca6c32837d9fc1242bbc8521
Seppe Dekeyser <seppe.dekeyser@hotmail.com>
2025-08-02 14:03:14 +0200

    notice on docs page with link to new docs

---
+++ site/src/components/VersionNotice.svelte
@@ -0,0 +1,35 @@
+<script>
+</script>
+
+<div class="version-notice-container">
+	<p>
+		Find the <code>houdini@next</code> docs
+		<a href="https://houdini-docs-next.netlify.app/guides/migrateTo20" target="_blank">here</a>
+	</p>
+</div>
+
+<style>
+	.version-notice-container {
+		top: 0;
+		width: 100%;
+		position: sticky;
+		z-index: 1000;
+		background: var(--experimental-background);
+		color: var(--experimental-text);
+	}
+
+	p {
+		font-size: 18px;
+		font-family: 'Hind', sans-serif;
+		font-weight: 500;
+		text-align: center;
+		padding: 0.5em;
+	}
+
+	a {
+		color: var(--experimental-text);
+		font-weight: bold;
+		text-decoration: underline;
+		white-space: nowrap;
+	}
+</style>
