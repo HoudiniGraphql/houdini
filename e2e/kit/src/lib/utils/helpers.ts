1efa894a8f95c5bcb50af338b80e10294fcd6f58
Alec Aivazis <alec@aivazis.com>
2025-10-03 23:35:29 -0700

    clean up deps

--- e2e/kit/src/lib/utils/helpers.ts
+++ e2e/kit/src/lib/utils/helpers.ts
@@ -1,17 +1,13 @@
 /**
- * Local implementations of utilities previously from @kitql/helpers
- */
-
-/**
  * Sleep function - promise-based wrapper over setTimeout
  */
 export async function sleep(ms: number): Promise<void> {
-	return new Promise((resolve) => setTimeout(resolve, ms))
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 /**
  * Stringify function - JSON.stringify with optional indentation
  */
 export function stry(obj: any, indent?: number): string {
-	return JSON.stringify(obj, null, indent)
+  return JSON.stringify(obj, null, indent);
 }
