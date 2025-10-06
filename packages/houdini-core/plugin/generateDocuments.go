dbea951d0bd96a3659e9571f39cc73f44ae14fe9
Alec Aivazis <alec@aivazis.com>
2025-10-06 00:46:05 -0700

    remove faff in generate documents method

--- packages/houdini-core/plugin/generateDocuments.go
+++ packages/houdini-core/plugin/generateDocuments.go
@@ -4,20 +4,9 @@ import (
 	"context"
 
 	"code.houdinigraphql.com/packages/houdini-core/plugin/documents/artifacts"
-	"code.houdinigraphql.com/plugins"
 )
 
 func (p *HoudiniCore) GenerateDocuments(ctx context.Context) ([]string, error) {
-	// we need to build up the filepaths that we generate in this time
-	generated := plugins.ThreadSafeSlice[string]{}
-
 	// the first thing to do is generate the artifacts
-	files, err := artifacts.Generate(ctx, p.DB, p.Fs, false)
-	if err != nil {
-		return nil, err
-	}
-	generated.Append(files...)
-
-	// we're done
-	return generated.GetItems(), nil
+	return artifacts.Generate(ctx, p.DB, p.Fs, false)
 }
