3971b5b970a9dca1c86f3c374f2be04479424268
Alec Aivazis <alec@aivazis.com>
2025-10-13 00:24:38 -0700

    only allow strings to graphql functino

--- packages/houdini-core/runtime/index.ts
+++ packages/houdini-core/runtime/index.ts
@@ -9,8 +9,8 @@ export * from './lib'
 // this template tag gets removed by the preprocessor so it should never be invoked.
 // this function must return any so that we can assign it a type in a variable declaration (ie an inline store)
 // ideally we would be able to parse the input for values but typescript does not yet support that kind of matches in template args
-export function graphql<_Payload, _Result = _Payload>(str: TemplateStringsArray): _Result
-export function graphql(str: string | TemplateStringsArray): never {
+export function graphql<_Payload, _Result = _Payload>(str: string): _Result
+export function graphql(str: string): never {
 	// if we are executing this function as part of the plugin, we need to return
 	// the query instead of throwing an error. We don't want to bundle the graphql
 	// module into the runtime so all we can do is return the query string
@@ -22,7 +22,7 @@ export function graphql(str: string | TemplateStringsArray): never {
 	}
 
 	// if this is executed, the preprocessor is not enabled
-	throw new Error(`⚠️ graphql template was invoked at runtime. This should never happen and usually means that your project isn't properly configured.
+	throw new Error(`! graphql template was invoked at runtime. This should never happen and usually means that your project isn't properly configured.
 
 Please make sure you have the appropriate plugin/preprocessor enabled. For more information, visit this link: https://houdinigraphql.com/guides/setting-up-your-project
 `)
