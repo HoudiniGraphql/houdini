1ceda6a5e3ed709d7f07256ac0f90b5e13f33651
Alec Aivazis <alec@aivazis.com>
2025-10-16 00:33:16 -0700

    add some store generation tests

---
+++ packages/houdini-svelte/plugin/generate_test.go
@@ -0,0 +1,97 @@
+package plugin_test
+
+import (
+	"context"
+	"testing"
+
+	"code.houdinigraphql.com/packages/houdini-svelte/plugin"
+	"code.houdinigraphql.com/plugins/tests"
+)
+
+func TestGenerate(t *testing.T) {
+	tests.RunTable(t, tests.Table[plugin.PluginConfig, *plugin.HoudiniSvelte]{
+		Schema: `
+			type User { 
+				id: ID!
+			}
+		}
+		`,
+		VerifyTest: func(t *testing.T, plugin *plugin.HoudiniSvelte, test tests.Test[plugin.PluginConfig]) {
+			config, err := plugin.DB.ProjectConfig(context.Background())
+		},
+		Tests: []tests.Test[plugin.PluginConfig]{
+			{
+				Name: "fragments",
+				Pass: true,
+				Input: []string{
+					`fragment TestFragment1 on User { id }`,
+					`fragment TestFragment2 on User { id }`,
+				},
+				Extra: map[string]any{
+					"TestFragment1": tests.Dedent(`
+							import { FragmentStore } from '../runtime/stores/fragment'
+							import artifact from '$houdini/artifacts/TestFragment1'
+
+
+							// create the fragment store
+
+							export class TestFragment1Store extends FragmentStore {
+								constructor() {
+									super({
+										artifact,
+										storeName: "TestFragment1Store",
+										variables: true,
+										
+									})
+								}
+							}
+					`),
+				},
+			},
+			{
+				Name: "mutations",
+				Pass: true,
+				Input: []string{
+					`mutation TestMutation1 { updateUser { id }  }`,
+					`mutation TestMutation2 { updateUser { id }  }`,
+				},
+				Extra: map[string]any{
+					"TestMutation1": tests.Dedent(`
+						import artifact from '$houdini/artifacts/TestMutation1'
+						import { MutationStore } from '../runtime/stores/mutation'
+
+						export class TestMutation1Store extends MutationStore {
+							constructor() {
+								super({
+									artifact,
+								})
+							}
+						}
+					`),
+				},
+			},
+			{
+				Name: "subscriptions",
+				Pass: true,
+				Input: []string{
+					`subscription TestSubscription1 { newUser { id } }`,
+					`subscription TestSubscription2 { newUser { id } }`,
+				},
+				Extra: map[string]any{
+					"TestSubscription1": tests.Dedent(`
+						import artifact from '$houdini/artifacts/TestSubscription1'
+						import { SubscriptionStore } from '../runtime/stores/subscription'
+
+						export class TestSubscription1Store extends SubscriptionStore {
+							constructor() {
+								super({
+									artifact,
+								})
+							}
+						}
+					`),
+				},
+			},
+		},
+	})
+}
---
+++ packages/houdini-svelte/runtime/session.ts
@@ -0,0 +1,69 @@
+import type { LoadEvent, RequestEvent } from '@sveltejs/kit'
+import { isBrowser } from './adapter'
+
+const sessionKeyName = '__houdini__session__'
+
+const sessionSentinel = {}
+// @ts-ignore
+let session: App.Session | {} = sessionSentinel
+
+export function extractSession(val: {
+	[sessionKeyName]: // @ts-ignore
+	App.Session
+}) {
+	return val[sessionKeyName]
+}
+
+export function buildSessionObject(event: RequestEvent) {
+	return {
+		[sessionKeyName]: extractSession(event.locals as any),
+	}
+}
+
+export function setClientSession(
+	// @ts-ignore
+	val: App.Session
+) {
+	if (!isBrowser) {
+		return
+	}
+
+	session = val
+}
+
+// @ts-ignore
+export function getClientSession(): App.Session {
+	return session
+}
+
+export function setSession(
+	event: RequestEvent,
+	session: // @ts-ignore
+	App.Session
+) {
+	;(event.locals as any)[sessionKeyName] = session
+}
+
+export async function getSession(event?: RequestEvent | LoadEvent): Promise<
+	| {}
+	// @ts-ignore
+	| App.Session
+> {
+	if (event) {
+		// get the session either from the server side event or the client side event
+		if ('locals' in event) {
+			// this is a server side event (RequestEvent) -> extract the session from locals
+			return extractSession(event.locals as any) || sessionSentinel
+		}
+		// the session data could also already be present in the data field
+		else if ('data' in event && event.data && sessionKeyName in event.data) {
+			// @ts-ignore
+			return extractSession(event.data) || sessionSentinel
+		} else {
+			// this is a client side event -> await the parent data which include the session
+			return extractSession((await event.parent()) as any) || sessionSentinel
+		}
+	}
+
+	return session
+}
