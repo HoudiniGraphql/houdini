68fbd738a6d1a331f73b4e09559d5b1568ee5021
Seppe Dekeyser <seppe.dekeyser@hotmail.com>
2026-06-05 15:10:56 +0200

    init houdini and vite config

--- packages/sv-addon/src/index.js
+++ packages/sv-addon/src/index.js
@@ -2,52 +2,149 @@ import { defineAddon, defineAddonOptions } from 'sv'
 import { transforms } from './sv-utils.js'
 
 const options = defineAddonOptions()
-	.add('who', {
-		question: 'To whom should the addon say hello?',
+	.add('is_remote_endpoint', {
+		question: 'Will you use a remote GraphQL API?',
+		type: 'boolean',
+		default: true,
+	})
+	.add('remote_endpoint', {
+		question: "What's the URL for your api?",
 		type: 'string',
 		default: '',
+		placeholder: 'http://localhost:4000/graphql',
+		validate: (value) => {
+			if (!value?.startsWith('http')) {
+				return 'Please enter a valid URL'
+			}
+		},
+		// Only ask if specified to be using a remote endpoint
+		condition: (opts) => opts.is_remote_endpoint === true,
+	})
+	.add('local_schema', {
+		question: "Where's your schema located?",
+		type: 'string',
+		default: '',
+		validate: (value) => {
+			if (value === '') {
+				return 'Please enter a valid schemaPath'
+			}
+		},
+		// Only ask if specified to be using a local schema
+		condition: (opts) => opts.is_remote_endpoint === false,
 	})
 	.build()
 
 export default defineAddon({
 	id: '@houdinigraphql/sv',
+	shortDescription: 'GraphQL client library',
+	homepage: 'https://houdinigraphql.com',
 	options,
 
 	setup: ({ isKit, unsupported }) => {
 		if (!isKit) unsupported('Requires SvelteKit')
 	},
 
+	nextSteps: () => ['Check the Houdini tutorial at https://houdinigraphql.com/intro'],
+
 	run: ({ directory, sv, options, language }) => {
-		sv.file(
-			`${directory.lib}/@houdinigraphql/sv/content.txt`,
-			transforms.text(() => {
-				return `This is a text file made by the Community Addon Template demo for the add-on: '@houdinigraphql/sv'!`
-			})
-		)
+		sv.dependency('houdini', '2.0.0-next.27')
+		sv.dependency('houdini-svelte', '3.0.0-next.14')
 
 		sv.file(
-			`${directory.lib}/@houdinigraphql/sv/HelloComponent.svelte`,
-			transforms.svelteScript({ language }, ({ ast, svelte, js }) => {
-				js.imports.addDefault(ast.instance.content, {
-					as: 'content',
-					from: './content.txt?raw',
+			'houdini.config.js',
+			transforms.script(({ ast, comments, content, js }) => {
+				// Don't overwrite if a config is already present
+				if (content) return false
+
+				ast.body.push(
+					js.variables.declaration(ast, {
+						kind: 'const',
+						name: 'config',
+						value: js.object.create({
+							watchSchema: options.is_remote_endpoint
+								? { url: options.remote_endpoint }
+								: undefined,
+							schemaPath: !options.is_remote_endpoint
+								? options.local_schema
+								: undefined,
+							runtimeDir: '.houdini',
+							plugins: {
+								'houdini-svelte': {},
+							},
+						}),
+					})
+				)
+
+				js.exports.createDefault(ast, {
+					fallback: js.variables.createIdentifier('config'),
 				})
-
-				svelte.addFragment(ast, '<p>{content}</p>')
-				svelte.addFragment(ast, `<h2>Hello ${options.who}!</h2>`)
 			})
 		)
+
+		// TODO:
+		// sv.file(
+		//   `${directory.src}/client.${language}`,
+		//   transforms.script(({ ast, comments, content, js }) => {
+		//     // Don't oerwrite if a client is already present
+		//     if (content) return false
+		//
+		//     js.imports.addNamed(ast, {
+		//       imports: ['HoudiniClient'],
+		//       from: '$houdini',
+		//     })
+		//
+		//     js.exports.createDefault(ast, {
+		//       fallback: js.object.create({
+		//         url: options.remote_endpoint
+		//       })
+		//     })
+		//   })
+		// )
 
 		sv.file(
-			directory.kitRoutes + '/+page.svelte',
-			transforms.svelteScript({ language }, ({ ast, svelte, js }) => {
-				js.imports.addDefault(ast.instance.content, {
-					as: 'HelloComponent',
-					from: `$lib/@houdinigraphql/sv/HelloComponent.svelte`,
+			`vite.config.${language}`,
+			transforms.script(({ ast, js }) => {
+				js.imports.addDefault(ast, {
+					from: 'houdini/vite',
+					as: 'houdini',
 				})
-
-				svelte.addFragment(ast, '<HelloComponent />')
+				js.vite.addPlugin(ast, {
+					code: 'houdini()',
+					mode: 'prepend',
+				})
 			})
 		)
+
+		// sv.file(
+		// 	`${directory.lib}/@houdinigraphql/sv/content.txt`,
+		// 	transforms.text(() => {
+		// 		return `This is a text file made by the Community Addon Template demo for the add-on: '@houdinigraphql/sv'!`
+		// 	})
+		// )
+		//
+		// sv.file(
+		// 	`${directory.lib}/@houdinigraphql/sv/HelloComponent.svelte`,
+		// 	transforms.svelteScript({ language }, ({ ast, svelte, js }) => {
+		// 		js.imports.addDefault(ast.instance.content, {
+		// 			as: 'content',
+		// 			from: './content.txt?raw',
+		// 		})
+		//
+		// 		svelte.addFragment(ast, '<p>{content}</p>')
+		// 		svelte.addFragment(ast, `<h2>Hello ${options.who}!</h2>`)
+		// 	})
+		// )
+		//
+		// sv.file(
+		// 	directory.kitRoutes + '/+page.svelte',
+		// 	transforms.svelteScript({ language }, ({ ast, svelte, js }) => {
+		// 		js.imports.addDefault(ast.instance.content, {
+		// 			as: 'HelloComponent',
+		// 			from: `$lib/@houdinigraphql/sv/HelloComponent.svelte`,
+		// 		})
+		//
+		// 		svelte.addFragment(ast, '<HelloComponent />')
+		// 	})
+		// )
 	},
 })
--- packages/sv-addon/tests/addon.test.js
+++ packages/sv-addon/tests/addon.test.js
@@ -11,8 +11,21 @@ const { test, prepareServer, testCases } = setupTest(
   { addon },
   {
     kinds: [
-      { type: 'remote', options: { [addon.id]: { who: 'you' } } }
-      { type: 'local', options: { [addon.id]: { who: 'you' } } }
+      {
+        type: 'remote', options: {
+          [addon.id]: {
+            is_remote_endpoint: true,
+            remote_endpoint: "http://localhost:4000/graphql"
+          }
+        }
+      },
+      {
+        type: 'local', options: {
+          [addon.id]: {
+            is_remote_endpoint: false, local_schema: "./schema.graphql"
+          }
+        }
+      }
     ],
     filter: (testCase) => testCase.variant.includes('kit'),
     browser
@@ -24,32 +37,43 @@ test.concurrent.for(testCases)(
   async (testCase, { page, ...ctx }) => {
     const cwd = ctx.cwd(testCase);
 
-    const msg = "Community Addon Template demo for the add-on: '@houdinigraphql/sv'!";
+    const configPath = path.resolve(cwd, 'houdini.config.js')
+    const configContent = fs.readFileSync(configPath, 'utf8');
 
-    const contentPath = path.resolve(cwd, `src/lib/@houdinigraphql/sv/content.txt`);
-    const contentContent = fs.readFileSync(contentPath, 'utf8');
-    // Check if we have the imports
-    expect(contentContent).toContain(msg);
-
-    const helloPath = path.resolve(cwd, `src/lib/@houdinigraphql/sv/HelloComponent.svelte`);
-    const helloContent = fs.readFileSync(helloPath, 'utf8');
-    // Check if we have the imports
-    expect(helloContent).toContain('you');
-
-    // For browser testing
-    if (browser) {
-      const { close } = await prepareServer({ cwd, page });
-      // kill server process when we're done
-      ctx.onTestFinished(async () => await close());
-
-      // expectations
-      const textContent = await page.locator('p').last().textContent();
-      if (testCase.variant.includes('kit')) {
-        expect(textContent).toContain(msg);
-      } else {
-        // it's not a kit plugin!
-        expect(textContent).not.toContain(msg);
-      }
+    if (testCase.kind.type === "remote") {
+      expect(configContent).toContain('watchSchema')
+      expect(configContent).toContain('http://localhost:4000/graphql')
+    } else if (testCase.kind.type === "local") {
+      expect(configContent).toContain('schemaPath')
+      expect(configContent).toContain('./schema.graphql')
     }
+
+    // const msg = "Community Addon Template demo for the add-on: '@houdinigraphql/sv'!";
+    //
+    // const contentPath = path.resolve(cwd, `src/lib/@houdinigraphql/sv/content.txt`);
+    // const contentContent = fs.readFileSync(contentPath, 'utf8');
+    // // Check if we have the imports
+    // expect(contentContent).toContain(msg);
+    //
+    // const helloPath = path.resolve(cwd, `src/lib/@houdinigraphql/sv/HelloComponent.svelte`);
+    // const helloContent = fs.readFileSync(helloPath, 'utf8');
+    // // Check if we have the imports
+    // expect(helloContent).toContain('you');
+    //
+    // // For browser testing
+    // if (browser) {
+    //   const { close } = await prepareServer({ cwd, page });
+    //   // kill server process when we're done
+    //   ctx.onTestFinished(async () => await close());
+    //
+    //   // expectations
+    //   const textContent = await page.locator('p').last().textContent();
+    //   if (testCase.variant.includes('kit')) {
+    //     expect(textContent).toContain(msg);
+    //   } else {
+    //     // it's not a kit plugin!
+    //     expect(textContent).not.toContain(msg);
+    //   }
+    // }
   }
 );
