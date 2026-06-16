0d85c8f9efc1933a701cfbca1a9f417c61b3c4ec
Alec Aivazis <alec@aivazis.com>
2026-06-15 20:01:16 -0700

    fix stale keyRaw argument order in paginate mode test snapshot

--- packages/houdini-core/plugin/documents/artifacts/selection_lists_test.go
+++ packages/houdini-core/plugin/documents/artifacts/selection_lists_test.go
@@ -806,7 +806,7 @@ export type TestQuery$artifact = typeof artifact
         "fields": {
             "usersByCursor": {
                 "type": "UserConnection",
-                "keyRaw": "usersByCursor(first: $first, after: $after, last: $last, before: $before)",
+                "keyRaw": "usersByCursor(after: $after, before: $before, first: $first, last: $last)",
 
                 "directives": [{
                     "name": "paginate",
